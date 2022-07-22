22b277d43bf3885df563b9940e97cc566cd706c9
cuhte3 <farid@getportal.co>
2022-07-22 19:11:58 +0700

    restoring extended key logic + descriptors

--- BDKDemoApp/ViewModels/WalletViewModel.swift
+++ BDKDemoApp/ViewModels/WalletViewModel.swift
@@ -44,13 +44,22 @@ class WalletViewModel: ObservableObject {
     
     func load() {
         state = .loading
+        
+        let stringsArray = ["fiscal", "ribbon", "chief", "chest", "truly", "rough", "woman", "ugly", "opera", "language", "raccoon", "victory", "expose", "elder", "asthma", "curious", "special", "cactus", "train", "equip", "exchange", "artist", "journey", "dish"]
+        let mnemonic = String(stringsArray.reduce(String(), { $0 + " " + $1}).dropFirst())
+        let restoredExtendedKey = try! restoreExtendedKey(network: Network.testnet, mnemonic: mnemonic, password: "salty_password")
+        let descriptor = "wpkh([\(restoredExtendedKey.fingerprint)/44'/0'/0']\(restoredExtendedKey.xprv)/*)"
+        let changeDescriptor = "wpkh([\(restoredExtendedKey.fingerprint)/44'/0'/1']\(restoredExtendedKey.xprv)/*)"
+        
+        print(restoredExtendedKey.mnemonic)
+        print(restoredExtendedKey.xprv)
+        
         let db = DatabaseConfig.memory
-        let descriptor = "wpkh(tprv8ZgxMBicQKsPeSitUfdxhsVaf4BXAASVAbHypn2jnPcjmQZvqZYkeqx7EHQTWvdubTSDa5ben7zHC7sUsx4d8tbTvWdUtHzR8uhHg2CW7MT/*)"
         let electrum = ElectrumConfig(url: "ssl://electrum.blockstream.info:60002", socks5: nil, retry: 5, timeout: nil, stopGap: 10)
         let blockchainConfig = BlockchainConfig.electrum(config: electrum)
         do {
             let blockchain = try Blockchain(config: blockchainConfig)
-            let wallet = try Wallet(descriptor: descriptor, changeDescriptor: nil, network: Network.testnet, databaseConfig: db)
+            let wallet = try Wallet(descriptor: descriptor, changeDescriptor: changeDescriptor, network: Network.testnet, databaseConfig: db)
             state = State.loaded(wallet, blockchain)
         } catch let error {
             state = State.failed(error)
