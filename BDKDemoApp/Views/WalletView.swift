813c39f8922ed666644be3121f8e3d42ca5bdb54
cuhte3 <farid@getportal.co>
2022-07-22 19:59:29 +0700

    ui improvements

--- BDKDemoApp/Views/WalletView.swift
+++ BDKDemoApp/Views/WalletView.swift
@@ -13,7 +13,7 @@ struct WalletView: View {
     var body: some View {
         VStack(spacing: 0) {
             BalanceView(balance: viewModel.balance)
-                .padding(.top, 20)
+                .padding(.top, 18)
                 .padding(.horizontal, 16)
             ActionButtonsView
                 .padding(.top, 28)
@@ -50,99 +50,126 @@ struct WalletView: View {
         HStack {
             VStack(alignment: .leading, spacing: 12) {
                 Text("total balance")
-                    .textStyle(BasicTextStyle(white: true))
-                HStack(alignment: .bottom, spacing: 1) {
+                    .font(.system(size: 16, design: .monospaced))//.weight(bold ? .bold : .regular))
+                    .foregroundColor(Color(red: 138/255, green: 138/255, blue: 138/255, opacity: 1))
+                HStack(alignment: .bottom, spacing: 0) {
                     if viewModel.isSynced {
                         Text("\(balance)")
-                            .fontWeight(.bold)
-                            .font(.system(size: 48))
+                            .font(.system(size: 48, design: .monospaced))
+                            .fontWeight(.semibold)
                         Text("sats")
-                            .font(.system(size: 16))
+                            .font(.system(size: 16, design: .monospaced))
+                            .foregroundColor(Color(red: 138/255, green: 138/255, blue: 138/255, opacity: 1))
                             .padding(.bottom, 10)
                     } else {
                         Text("Syncing...")
-                            .fontWeight(.bold)
-                            .font(.system(size: 48))
+                            .font(.system(size: 48, design: .monospaced))
+                            .fontWeight(.semibold)
                     }
                 }
+                .frame(height: 32)
             }
             Spacer()
         }
     }
     
     var ActionButtonsView: some View {
-        HStack {
+        HStack(spacing: 10) {
             Button {
                 
             } label: {
                 Text("Send")
-                    .font(.system(size: 18))
+                    .foregroundColor(.black)
+                    .font(.system(size: 16))
                     .fontWeight(.semibold)
                     .frame(maxWidth: .infinity)
                     .padding(8)
                     .background(Color.blue)
-                    .background(in: RoundedRectangle(cornerRadius: 8))
+                    .background(in: RoundedRectangle(cornerRadius: 10))
+                    .frame(height: 30)
             }
             .buttonStyle(.plain)
+            .frame(maxWidth: .infinity)
+            .frame(height: 30)
 
             Button {
                 
             } label: {
                 Text("Receive")
-                    .font(.system(size: 18))
+                    .foregroundColor(.black)
+                    .font(.system(size: 16))
                     .fontWeight(.semibold)
                     .frame(maxWidth: .infinity)
                     .padding(8)
                     .background(Color.blue)
-                    .background(in: RoundedRectangle(cornerRadius: 8))
+                    .background(in: RoundedRectangle(cornerRadius: 10))
+                    .frame(height: 30)
             }
             .frame(maxWidth: .infinity)
             .buttonStyle(.plain)
+            .frame(height: 30)
 
             Button {
                 
             } label: {
-                Text("[ ]")
+                Text("Scan")
+                    .foregroundColor(.black)
+                    .font(.system(size: 16))
                     .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity)
                     .padding(8)
                     .background(Color.blue)
-                    .background(in: RoundedRectangle(cornerRadius: 8))
+                    .background(in: RoundedRectangle(cornerRadius: 10))
+                    .frame(height: 30)
             }
             .buttonStyle(.plain)
+            .frame(maxWidth: .infinity)
+            .frame(height: 30)
         }
     }
     
     func WalletItemView(item: WalletItem) -> some View {
-        VStack {
+        VStack(spacing: 12) {
             HStack(spacing: 4.8) {
                 RoundedRectangle(cornerRadius: 8)
                     .frame(width: 16, height: 16)
                     .foregroundColor(Color.blue)
                 Text(item.description)
-                    .font(.system(size: 16))
+                    .font(.system(size: 14.5))
+                    .foregroundColor(Color(red: 202/255, green: 202/255, blue: 202/255, opacity: 1))
                 Spacer()
             }
             HStack(alignment: .bottom) {
                 HStack(alignment: .bottom, spacing: 1) {
                     Text("\(item.balance)")
-                        .font(.system(size: 32))
+                        .font(.system(size: 32, design: .monospaced))
                         .fontWeight(.semibold)
                     Text("sats")
+                        .font(.system(size: 16, design: .monospaced))
+                        .foregroundColor(Color(red: 138/255, green: 138/255, blue: 138/255, opacity: 1))
                         .padding(.bottom, 6)
                 }
+                .frame(height: 32)
                 Spacer()
                 HStack(spacing: 1) {
                     Text("$\(item.fiatValue)")
+                        .font(.system(size: 16, design: .monospaced))
+                        .foregroundColor(Color(red: 202/255, green: 202/255, blue: 202/255, opacity: 1))
                     Text("USD")
+                        .font(.system(size: 16, design: .monospaced))
+                        .foregroundColor(Color(red: 138/255, green: 138/255, blue: 138/255, opacity: 1))
                 }
                 .padding(.bottom, 6)
             }
         }
+        .frame(height: 84)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        WalletView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        WalletView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+            .preferredColorScheme(.dark)
     }
 }
