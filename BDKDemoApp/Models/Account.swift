1c3f06cd38bf3b3eaaccc4b511829b3b7e7277a4
cuhte3 <farid@getportal.co>
2022-08-09 21:36:33 +0700

    Create Account.swift

---
+++ BDKDemoApp/Models/Account.swift
@@ -0,0 +1,60 @@
+//
+//  Account.swift
+//  Portal
+//
+//  Created by Farid on 19.07.2021.
+//
+
+import Foundation
+import BitcoinDevKit
+
+class Account: IAccount {
+    let id: String
+    let index: Int
+    let extendedKey: ExtendedKeyInfo
+
+    private(set) var name: String
+    
+    private let btcNetwork: Int
+    private let ethNetwork: Int
+
+    init(id: String, index: Int, name: String, key: ExtendedKeyInfo) {
+        self.id = id
+        self.index = index
+        self.name = name
+        self.btcNetwork = 1 //testNet
+        self.ethNetwork = 1 //ropsten
+        self.extendedKey = key
+    }
+    
+    init(record: AccountRecord, key: ExtendedKeyInfo) {
+        self.id = record.id
+        self.index = Int(record.index)
+        self.name = record.name
+        self.btcNetwork = 1 //testNet
+        self.ethNetwork = 1 //ropsten
+        self.extendedKey = key
+    }
+}
+
+extension Account: Hashable {
+    public static func ==(lhs: Account, rhs: Account) -> Bool {
+        lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+class MockedAccount: IAccount {
+    var index: Int = 0
+    
+    var id: String {
+        UUID().uuidString
+    }
+    
+    var name: String {
+        "Mocked"
+    }
+}
