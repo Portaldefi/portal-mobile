846a450c488125d1fd671a4836d6d01bb17ae743
cuhte3 <farid@getportal.co>
2022-08-09 21:37:30 +0700

    Update BDKDemoAppApp.swift

--- BDKDemoApp/BDKDemoAppApp.swift
+++ BDKDemoApp/BDKDemoAppApp.swift
@@ -13,7 +13,7 @@ struct BDKDemoAppApp: App {
 
     var body: some Scene {
         WindowGroup {
-            HostingTabBarView()
+            RootView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
                 .preferredColorScheme(.dark)
         }
