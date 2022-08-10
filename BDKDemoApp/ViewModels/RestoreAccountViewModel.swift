2c607eed4b7aa983c4b5541acad8b9e4fadea038
farid <farid@getportal.co>
2022-08-11 00:39:07 +0700

    Update RestoreAccountViewModel.swift

--- BDKDemoApp/ViewModels/RestoreAccountViewModel.swift
+++ BDKDemoApp/ViewModels/RestoreAccountViewModel.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 import Combine
+import BitcoinDevKit
 
 class RestoreAccountViewModel: ObservableObject {
     @Published var accountName = String()
     @Published var seed = String()
-    @Published var restorable: Bool = false
+    @Published var restorable = false
     
     private var subscriptions = Set<AnyCancellable>()
     
@@ -26,6 +27,8 @@ class RestoreAccountViewModel: ObservableObject {
     
     func restoreAccount() {
         let words = seed.components(separatedBy: " ").filter{ !$0.isEmpty && $0.count >= 3 }
-        print(words)
+        let restoredKey = try! restoreExtendedKey(network: Network.testnet, mnemonic: words.joined(separator:" "), password: nil)
+        let account = Account(id: UUID().uuidString, index: 0, name: accountName, key: restoredKey)
+        Portal.shared.accountManager.save(account: account)
     }
 }
