0597c0b253053997124b342bd107eb29eb5bed8d
farid <farid@getportal.co>
2022-08-19 16:58:47 +0700

    Update QRCodeScannerView.swift

--- BDKDemoApp/Views/QRCodeScannerView.swift
+++ BDKDemoApp/Views/QRCodeScannerView.swift
@@ -10,20 +10,29 @@ import CodeScanner
 import PortalUI
 
 struct QRCodeScannerView: View {
+    @State private var overlayOpacity: Double = 0
+    
     var body: some View {
         ZStack {
             Color(red: 26/255, green: 26/255, blue: 26/255, opacity: 1).ignoresSafeArea()
             
             VStack {
                 ZStack {
-                    CodeScannerView(codeTypes: [.qr], scanMode: .continuous, simulatedData: "sajdhsadjgewriusdakdjhsaiufewkjhfg") { response in
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "bitcoin:BC1QYLH3U67J673H6Y6ALV70M0PL2YZ53TZHVXGG7U?amount=0.00001&label=sbddesign%3A%20For%20lunch%20Tuesday&message=For%20lunch%20Tuesday&lightning=LNO1PG257ENXV4EZQCNEYPE82UM50YNHXGRWDAJX283QFWDPL28QQMC78YMLVHMXCSYWDK5WRJNJ36JRYG488QWLRNZYJCZS") { response in
                         if case let .success(result) = response {
-                            print(response)
-                            //scannedCode = result.string
-                            //isPresentingScanner = false
+                            let items = QRCodeParser.current.parse(result.string)
+                            for item in items {
+                                print(item)
+                            }
                         }
                     }
                     CameraTargetOverlayView()
+                        .opacity(overlayOpacity)
+                        .onAppear {
+                            withAnimation {
+                                overlayOpacity = 1
+                            }
+                        }
                 }
                 .cornerRadius(12)
                 .padding()
