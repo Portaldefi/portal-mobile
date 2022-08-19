5ca117a9ceb25cf6d5464999f5e20ce91517267d
farid <farid@getportal.co>
2022-08-19 16:57:50 +0700

    Create QRCodeItem.swift

---
+++ BDKDemoApp/Models/QRCodeItem.swift
@@ -0,0 +1,41 @@
+//
+//  QRCodeItem.swift
+//  BDKDemoApp
+//
+//  Created by farid on 19/8/22.
+//
+
+import Foundation
+
+struct QRCodeItem {
+    enum ItemType {
+        case bip21(address: String, amount: String?, message: String?),
+             bolt11(invoice: String),
+             bolt12(offer: String),
+             pubKey(xpub: String),
+             privKey(key: String),
+             unsupported
+    }
+    
+    let type: ItemType
+    
+    static var unsupported: QRCodeItem {
+        QRCodeItem(type: .unsupported)
+    }
+    
+    static func bip21(address: String, amount: String? = nil, message: String? = nil) -> QRCodeItem {
+        QRCodeItem(type: .bip21(address: address, amount: amount, message: message))
+    }
+    
+    static func bolt11(invoice: String) -> QRCodeItem {
+        QRCodeItem(type: .bolt11(invoice: invoice))
+    }
+    
+    static func publicKey(xpub: String) -> QRCodeItem {
+        QRCodeItem(type: .pubKey(xpub: xpub))
+    }
+    
+    static func privateKey(key: String) -> QRCodeItem {
+        QRCodeItem(type: .privKey(key: key))
+    }
+}
