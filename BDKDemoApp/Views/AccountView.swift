a061c1f24e7c01de633c7cb3a49311f723781f2d
farid <farid@getportal.co>
2022-08-25 18:31:01 +0700

    Update AccountView.swift

--- BDKDemoApp/Views/AccountView.swift
+++ BDKDemoApp/Views/AccountView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import PortalUI
+import Factory
 
 struct AccountView: View {
     @State private var goToTxs = false
@@ -15,8 +16,8 @@ struct AccountView: View {
     @State private var qrScannerOpened = false
     @State private var qrItem: QRCodeItem? = nil
     
-    @EnvironmentObject private var viewState: ViewState
-    @EnvironmentObject private var viewModel: AccountViewModel
+    @ObservedObject private var viewModel = Container.accountViewModel()
+    @ObservedObject private var viewState = Container.viewState()
     
     init() {
         UINavigationBar
@@ -40,7 +41,12 @@ struct AccountView: View {
                 VStack { Text("DB not founded...") }
             case .loaded:
                 ZStack {
-                    Color(red: 26/255, green: 26/255, blue: 26/255, opacity: 1)
+                    Color(
+                        red: 26/255,
+                        green: 26/255,
+                        blue: 26/255,
+                        opacity: 1
+                    )
                     
                     VStack(spacing: 0) {
                         Group {
@@ -55,7 +61,6 @@ struct AccountView: View {
                                 .padding(.bottom, 24)
                         }
                         .background(Color(red: 16/255, green: 16/255, blue: 16/255, opacity: 1))
-
                         
                         ScrollView {
                             VStack(spacing: 0) {
@@ -72,10 +77,14 @@ struct AccountView: View {
                                         .overlay(.black)
                                 }
                             }
-                            NavigationLink(destination: TxsView(txs: viewModel.transactions), isActive: $goToTxs) { EmptyView() }
-                            NavigationLink(destination: SendView(item: qrItem), isActive: $goToSend) { EmptyView() }
+                            NavigationLink(
+                                destination: TxsView(txs: viewModel.transactions),
+                                isActive: $goToTxs
+                            ) {
+                                EmptyView()
+                            }
+//                            NavigationLink(destination: SendView(item: qrItem), isActive: $goToSend) { EmptyView() }
                         }
-                        
                         Spacer()
                     }
                     .navigationBarHidden(true)
@@ -93,6 +102,11 @@ struct AccountView: View {
                 qrItem = item
                 goToSend.toggle()
             }
+        }
+        .sheet(isPresented: $goToSend, onDismiss: {
+            
+        }) {
+            SendView(walletItems: viewModel.items, qrCodeItem: qrItem)
         }
     }
     
@@ -175,74 +189,13 @@ struct AccountView: View {
                 size: .medium,
                 enabled: viewModel.syncState == .synced
             ) {
-                goToSend.toggle()
+                withAnimation {
+                    goToSend.toggle()
+                }
             }
             
             NavigationLink(destination: ReceiveView(viewModel: viewModel), isActive: $goToReceive) { EmptyView() }
         }
-    }
-    
-    func WalletItemView(item: WalletItem) -> some View {
-        HStack {
-            VStack(spacing: 12) {
-                VStack(spacing: 4.2) {
-                    HStack(spacing: 6) {
-                        RoundedRectangle(cornerRadius: 8)
-                            .frame(width: 16, height: 16)
-                            .foregroundColor(Color.green)
-                        Text("Bitcoin")
-                            .font(.system(size: 14, design: .monospaced))
-                            .fontWeight(.bold)
-                            .foregroundColor(Color(red: 202/255, green: 202/255, blue: 202/255, opacity: 1))
-                            .frame(height: 16)
-                        Spacer()
-                    }
-                    HStack(spacing: 2) {
-                        Spacer()
-                            .frame(width: 16, height: 16)
-                        Text("on")
-                            .font(.system(size: 12, design: .monospaced))
-                            .fontWeight(.bold)
-                            .foregroundColor(Color(red: 106/255, green: 106/255, blue: 106/255, opacity: 1))
-                            .frame(height: 17)
-                        Asset.chainIcon.resizable()
-                            .frame(width: 12, height: 12)
-                            .foregroundColor(Color(red: 138/255, green: 138/255, blue: 138/255, opacity: 1))
-                        Text("Chain")
-                            .font(.system(size: 12, design: .monospaced))
-                            .fontWeight(.bold)
-                            .foregroundColor(Color(red: 106/255, green: 106/255, blue: 106/255, opacity: 1))
-                            .frame(height: 17)
-                        Spacer()
-                    }
-                }
-            }
-            Spacer()
-            VStack(spacing: 0) {
-                HStack(spacing: 10) {
-                    Spacer()
-                    Text("\(item.balance)")
-                        .font(.system(size: 18, design: .monospaced))
-                        .fontWeight(.semibold)
-                    Text("sats")
-                        .font(.system(size: 16, design: .monospaced))
-                }
-                .foregroundColor(Color(red: 202/255, green: 202/255, blue: 202/255, opacity: 1))
-                .frame(height: 22)
-                
-                HStack(spacing: 10) {
-                    Spacer()
-                    Text(item.value)
-                        .font(.system(size: 14, design: .monospaced))
-                        .foregroundColor(Color(red: 106/255, green: 106/255, blue: 106/255, opacity: 1))
-                    Text("usd")
-                        .font(.system(size: 12, design: .monospaced))
-                        .foregroundColor(Color(red: 106/255, green: 106/255, blue: 106/255, opacity: 1))
-                }
-                .frame(height: 22)
-            }
-        }
-        .frame(height: 66)
     }
 }
 
