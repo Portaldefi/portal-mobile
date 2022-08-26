a99e45871b6d1f24228b7e19aa834fb035a2953a
farid <farid@getportal.co>
2022-08-26 19:22:06 +0700

    Update SendViewViewModel.swift

--- BDKDemoApp/ViewModels/SendViewViewModel.swift
+++ BDKDemoApp/ViewModels/SendViewViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Factory
 import BitcoinAddressValidator
+import Combine
 
 class SendViewViewModel: ObservableObject {
     var walletItems: [WalletItem] = []
@@ -19,8 +20,11 @@ class SendViewViewModel: ObservableObject {
     @Published var showErrorAlert = false
     @Published var selectedItem: WalletItem?
     @Published var qrCodeItem: QRCodeItem?
+    @Published var goToSend = false
     
     @Published private(set) var sendError: Error?
+    
+    private var subscriptions = Set<AnyCancellable>()
     
     @Injected(Container.accountViewModel) private var account
     
@@ -30,6 +34,26 @@ class SendViewViewModel: ObservableObject {
     
     init() {
         self.walletItems = account.items
+        
+        $selectedItem.sink { [unowned self] item in
+            self.goToSend = item != nil
+        }
+        .store(in: &subscriptions)
+        
+        $qrCodeItem.sink { [unowned self] item in
+            guard let item = item else { return }
+            switch item.type {
+            case .bip21(let address, let amount, _):
+                self.selectedItem = walletItems.first
+                self.to = address
+                guard let amount = amount else { return }
+                self.amount = amount
+            default:
+                break
+            }
+
+        }
+        .store(in: &subscriptions)
     }
     
     func send() {
@@ -41,18 +65,5 @@ class SendViewViewModel: ObservableObject {
             self?.sendError = error
             self?.showErrorAlert.toggle()
         })
-    }
-    
-    func set(item: QRCodeItem?) {
-        guard let i = item else { return }
-        switch i.type {
-        case .bip21(let address, let amount, _):
-            selectedItem = walletItems.first
-            to = address
-            guard let amount = amount else { return }
-            self.amount = amount
-        default:
-            break
-        }
     }
 }
