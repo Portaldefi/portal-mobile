7f9cf1dd790ba99599052223940d13c4512ba45f
farid <farid@getportal.co>
2022-08-26 21:06:09 +0700

    Update Portal+Injection.swift

--- BDKDemoApp/Portal+Injection.swift
+++ BDKDemoApp/Portal+Injection.swift
@@ -29,7 +29,7 @@ extension SharedContainer {
         return AccountManager(accountStorage: accountStorage)
     }
     
-    static let sendViewModel = Factory<SendViewViewModel> {
+    static let sendViewModel = Factory<SendViewViewModel>(scope: .shared) {
         SendViewViewModel()
     }
     
