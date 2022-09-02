26845191904dee08c0e0a572364ab33d55a8782e
cuhte3 <farid@getportal.co>
2022-09-02 16:54:18 +0700

    Update BiometricAuthentication.swift

--- BDKDemoApp/Models/BiometricAuthentication.swift
+++ BDKDemoApp/Models/BiometricAuthentication.swift
@@ -12,7 +12,34 @@ import Combine
 class BiometricAuthentication: ObservableObject {
     private let context = LAContext()
     
+    // .deviceOwnerAuthentication allows
+    // biometric or passcode authentication
+    private let authenticationPolicy: LAPolicy = .deviceOwnerAuthentication
+    
+    @Published private(set) var biometryType: LABiometryType
+    @Published private(set) var permissionsGranted: Bool
+    private var policyError: NSError?
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
     init() {
+        biometryType = context.biometryType
         
+        permissionsGranted = context.canEvaluatePolicy(
+            authenticationPolicy,
+            error: &policyError
+        )
+    }
+    
+    func authenticateUser(_ completion: @escaping (Bool, LAError?) -> ()) {
+        context.evaluatePolicy(
+            authenticationPolicy,
+            localizedReason: "Authenticate to continue"
+        ) { success, error in
+            guard let error = error as? LAError else {
+                return completion(success, nil)
+            }
+            completion(success, error)
+        }
     }
 }
