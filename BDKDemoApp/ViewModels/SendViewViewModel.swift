74c22ed3903fcd0022961f2f322839e3969aeaa4
cuhte3 <farid@getportal.co>
2022-09-02 16:55:03 +0700

    Update SendViewViewModel.swift
    func authenticateUser(_ completion: @escaping (Bool) -> ()) added + lazy injected auth service

--- BDKDemoApp/ViewModels/SendViewViewModel.swift
+++ BDKDemoApp/ViewModels/SendViewViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 import Factory
 import BitcoinAddressValidator
 import Combine
+import LocalAuthentication.LAError
 
 class SendViewViewModel: ObservableObject {
     var walletItems: [WalletItem] = []
@@ -28,6 +29,7 @@ class SendViewViewModel: ObservableObject {
     private var subscriptions = Set<AnyCancellable>()
     
     @Injected(Container.accountViewModel) private var account
+    @LazyInjected(Container.biometricAuthentification) private var biometrics
     
     var sendButtonEnabled: Bool {
         BitcoinAddressValidator.isValid(address: to) && (Double(amount) ?? 0) > 0
@@ -66,5 +68,61 @@ class SendViewViewModel: ObservableObject {
             self?.sendError = error
             self?.showErrorAlert.toggle()
         })
+    }
+    
+    func authenticateUser(_ completion: @escaping (Bool) -> ()) {
+        biometrics.authenticateUser { success, error in
+            guard let error = error else {
+                return completion(success)
+            }
+
+            switch error {
+            case LAError.appCancel:
+                // The app canceled authentication by
+                // invalidating the LAContext
+                completion(success)
+            case LAError.authenticationFailed:
+                // The user did not provide
+                // valid credentials
+                completion(success)
+            case LAError.invalidContext:
+                // The LAContext was invalid
+                completion(success)
+            case LAError.notInteractive:
+                // Interaction was not allowed so the
+                // authentication failed
+                completion(success)
+            case LAError.passcodeNotSet:
+                // The user has not set a passcode
+                // on this device
+                completion(success)
+            case LAError.systemCancel:
+                // The system canceled authentication,
+                // for example to show another app
+                completion(success)
+            case LAError.userCancel:
+                // The user canceled the
+                // authentication dialog
+                completion(false)
+            case LAError.userFallback:
+                // The user selected to use a fallback
+                // authentication method
+                completion(success)
+            case LAError.biometryLockout:
+                // Too many failed attempts locked
+                // biometric authentication
+                completion(success)
+            case LAError.biometryNotAvailable:
+                // The user's device does not support
+                // biometric authentication
+                completion(true)
+            case LAError.biometryNotEnrolled:
+                // The user has not configured
+                // biometric authentication
+                completion(true)
+            default:
+                completion(success)
+            }            
+        }
     }
 }
