7431645234887253ffcf696f589f7489c93c0c09
cuhte3 <farid@getportal.co>
2022-09-02 15:14:38 +0700

    Create BiometricAuthentication.swift

---
+++ BDKDemoApp/Models/BiometricAuthentication.swift
@@ -0,0 +1,18 @@
+//
+//  BiometricAuthentication.swift
+//  BDKDemoApp
+//
+//  Created by farid on 9/2/22.
+//
+
+import Foundation
+import LocalAuthentication
+import Combine
+
+class BiometricAuthentication: ObservableObject {
+    private let context = LAContext()
+    
+    init() {
+        
+    }
+}
