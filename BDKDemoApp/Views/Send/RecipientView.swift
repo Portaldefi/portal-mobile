4cc60e89a5d2863e20704802a27f3b97ce29cb52
cuhte3 <farid@getportal.co>
2022-09-12 17:10:29 +0700

    RecipientView init

---
+++ BDKDemoApp/Views/Send/RecipientView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipientView.swift
+//  BDKDemoApp
+//
+//  Created by farid on 9/12/22.
+//
+
+import SwiftUI
+
+struct RecipientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipientView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipientView()
+    }
+}
