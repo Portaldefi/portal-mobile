5ced4a0c6b1c4c3e817450d31f2032d8a4b35905
cuhte3 <farid@getportal.co>
2022-09-12 17:11:28 +0700

    SetAmountView init

---
+++ BDKDemoApp/Views/Send/SetAmountView.swift
@@ -0,0 +1,20 @@
+//
+//  SetAmountView.swift
+//  BDKDemoApp
+//
+//  Created by farid on 9/12/22.
+//
+
+import SwiftUI
+
+struct SetAmountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetAmountView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetAmountView()
+    }
+}
