17ef7313259fa2abcd3eded55943c9210498075d
cuhte3 <farid@getportal.co>
2022-09-13 16:36:03 +0700

    Update SendView.swift

--- BDKDemoApp/Views/Send/SendView.swift
+++ BDKDemoApp/Views/Send/SendView.swift
@@ -12,145 +12,70 @@ import Factory
 struct SendView: View {
     @FocusState private var isFocused: Bool
     @Environment(\.presentationMode) private var presentationMode
-    @ObservedObject var viewModel = Container.sendViewModel()
+    @ObservedObject var viewModel: SendViewViewModel = Container.sendViewModel()
     @Injected(Container.viewState) private var viewState
     
     var body: some View {
         ZStack {
-            Color(red: 10/255, green: 10/255, blue: 10/255).ignoresSafeArea()
+            Palette.grayScale0A.ignoresSafeArea()
             
             VStack(spacing: 0) {
                 ZStack {
                     HStack {
                         PButton(config: .onlyIcon(Asset.caretLeftIcon), style: .free, size: .medium, enabled: true) {
-                            presentationMode.wrappedValue.dismiss()
+                            withAnimation {
+                                if viewModel.goBack() {
+                                    presentationMode.wrappedValue.dismiss()
+                                }
+                            }
                         }
                         .frame(width: 20)
                         
                         Spacer()
-                        
-                        if !viewState.showScanner {
-                            PButton(config: .onlyIcon(Asset.qrIcon), style: .free, size: .medium, enabled: true) {
-                                viewModel.qrScannerOpened.toggle()
-                            }
-                            .frame(width: 25, height: 25)
-                        }
                     }
                     
-                    Text("Send")
+                    Text(viewModel.title)
+                        .frame(width: 300, height: 62)
                         .font(.Main.fixed(.bold, size: 16))
-                        .frame(height: 62)
+                        .animation(nil)
                 }
                 
-                VStack(spacing: 16) {
-                    VStack(alignment: .leading, spacing: 8) {
-                        if let item = viewModel.selectedItem {
-                            Text("From")
-                                .font(.system(size: 14, weight: .bold, design: .monospaced))
-                            
-                            WalletItemView(item: item)
-                                .padding(.horizontal)
-                                .contentShape(Rectangle())
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .stroke(Color(red: 42/255, green: 42/255, blue: 42/255), lineWidth: 1)
-                                )
-                        }
-                    }
-                    
-                    VStack(alignment: .leading, spacing: 8) {
-                        HStack {
-                            Text("Amount")
-                                .font(.system(size: 14, weight: .bold, design: .monospaced))
-                            Spacer()
-                            PButton(
-                                config: .onlyLabel("Max"),
-                                style: .free,
-                                size: .small,
-                                color: Color(red: 116/255, green: 138/255, blue: 254/255, opacity: 1),
-                                enabled: false
-                            ) {
-                                
-                            }
-                            .frame(width: 40)
-                        }
-                        
-                        TextField("Required", text: $viewModel.amount)
-                            .focused($isFocused)
-                            .disableAutocorrection(true)
-                            .textInputAutocapitalization(.never)
-                            .keyboardType(.decimalPad)
-                            .font(Font.system(size: 16, weight: .bold, design: .monospaced))
-                            .padding()
-                            .background(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .fill(
-                                        Color(red: 26/255, green: 26/255, blue: 26/255)
-                                    )
-                            )
-                    }
-                    
-                    VStack(alignment: .leading, spacing: 8) {
-                        HStack {
-                            Text("Address")
-                                .font(.system(size: 14, weight: .bold, design: .monospaced))
-                            Spacer()
-                        }
-                        
-                        ZStack {
-                            HStack {
-                                TextField("Required", text: $viewModel.to)
-                                    .focused($isFocused)
-                                    .disableAutocorrection(true)
-                                    .textInputAutocapitalization(.never)
-                                    .font(Font.system(size: 16, weight: .bold, design: .monospaced))
-                            }
-                            .padding()
-                        }
-                        .background(
-                            RoundedRectangle(cornerRadius: 12)
-                                .fill(
-                                    Color(red: 26/255, green: 26/255, blue: 26/255)
-                                )
-                        )
-                        
-                        HStack {
-                            PButton(config: .labelAndIconLeft(label: "Annotate", icon: Asset.pencilIcon), style: .free, size: .small, enabled: false) {
-                                
-                            }
-                            PButton(config: .labelAndIconLeft(label: "Tag", icon: Asset.tagIcon), style: .free, size: .small, enabled: false) {
-                                
-                            }
-                        }
-                        .padding()
-                    }
-                    
-                    Button {
-                        viewModel.goToReview.toggle()
-                    } label: {
-                        Text("Continue")
-                            .foregroundColor(.black)
-                            .font(.system(size: 22, design: .monospaced))
-                            .fontWeight(.bold)
-                            .frame(maxWidth: .infinity)
-                            .padding(16)
-                            .background(Color.blue)
-                            .background(in: RoundedRectangle(cornerRadius: 10))
-                            .frame(height: 60)
-                    }
-                    .disabled(!viewModel.sendButtonEnabled)
-                    .buttonStyle(.plain)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 60)
-                }
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    isFocused = false
+                switch viewModel.step {
+                case .recipient:
+                    SetRecipientView(viewModel: viewModel)
+                        .transition(.slide.combined(with: .scale))
+                        .transition(.move(edge: .trailing).combined(with: .opacity))
+                case .amount:
+                    SetAmountView()
+                        .transition(.slide.combined(with: .scale))
+                        .transition(.move(edge: .trailing).combined(with: .opacity))
+                case .review:
+                    Text("Review")
+                        .transition(.slide.combined(with: .scale))
+                        .transition(.move(edge: .trailing).combined(with: .opacity))
+                case .confirmation:
+                    Text("Confirmation")
+                        .transition(.slide.combined(with: .scale))
+                        .transition(.move(edge: .trailing).combined(with: .opacity))
                 }
                 
                 Spacer()
+                
+                switch viewModel.step {
+                case .recipient, .amount, .review:
+                    PButton(config: .onlyLabel(viewModel.actionButtonTitle), style: .filled, size: .big, enabled: viewModel.actionButtonEnabled) {
+                        withAnimation {
+                            viewModel.onActionButtonPressed()
+                        }
+                    }
+                case .confirmation:
+                    PButton(config: .onlyLabel("Done"), style: .outline, size: .big, enabled: true) {
+                        
+                    }
+                }
             }
             .padding(.horizontal, 16)
+            .padding(.bottom, 8)
             
             NavigationLink(
                 destination: ReviewTransactionView(),
@@ -161,7 +86,7 @@ struct SendView: View {
         }
         .navigationBarHidden(true)
         .sheet(isPresented: $viewModel.qrScannerOpened, onDismiss: {
-            
+            viewModel.qrScannerOpened = false
         }) {
             QRCodeReaderView(config: .send) { item in
                 viewModel.qrCodeItem = item
