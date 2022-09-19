053e905082274687eadd994303aa8c943ce2ea86
cuhte3 <farid@getportal.co>
2022-09-19 18:55:46 +0700

    Create ITransactionAdapter.swift

---
+++ BDKDemoApp/Protocols/ITransactionAdapter.swift
@@ -0,0 +1,19 @@
+//
+//  ITransactionAdapter.swift
+//  Portal
+//
+//  Created by Farid on 13.07.2021.
+//
+
+import Foundation
+import Combine
+import BitcoinDevKit
+
+protocol ITransactionsAdapter {
+    var coin: Coin { get }
+    var transactionState: AdapterState { get }
+    var transactionStateUpdated: AnyPublisher<Void, Never> { get }
+    var transactionRecords: AnyPublisher<[BitcoinDevKit.Transaction], Never> { get }
+    func transactions(from: BitcoinDevKit.Transaction?, limit: Int) -> Future<[BitcoinDevKit.Transaction], Never>
+    func rawTransaction(hash: String) -> String?
+}
