dbd38fac0aca492e5a0db36e48d445f7529049d9
cuhte3 <farid@getportal.co>
2022-09-21 11:49:27 +0700

    Update AdapterState.swift

--- BDKDemoApp/Models/AdapterState.swift
+++ BDKDemoApp/Models/AdapterState.swift
@@ -7,27 +7,15 @@
 
 import Foundation
 
-enum AdapterState {
+enum AdapterState: Equatable {
+    case empty
+    case syncing
     case synced
-    case syncing(progress: Int, lastBlockDate: Date?)
-    case searchingTxs(count: Int)
-    case notSynced(error: Error)
-
-    var isSynced: Bool {
-        switch self {
-        case .synced: return true
-        default: return false
-        }
-    }
-
-}
-
-extension AdapterState: Equatable {
-    public static func ==(lhs: AdapterState, rhs: AdapterState) -> Bool {
+    case failed(Error)
+    
+    static func ==(lhs: AdapterState, rhs: AdapterState) -> Bool {
         switch (lhs, rhs) {
-        case (.synced, .synced): return true
-        case (.syncing(let lProgress, let lLastBlockDate), .syncing(let rProgress, let rLastBlockDate)): return lProgress == rProgress && lLastBlockDate == rLastBlockDate
-        case (.notSynced, .notSynced): return true
+        case (.empty, .empty),(.syncing, .syncing), (.synced, .synced), (.failed, .failed) : return true
         default: return false
         }
     }
