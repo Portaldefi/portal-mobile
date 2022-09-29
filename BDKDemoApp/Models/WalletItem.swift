fe28494b046a9aae6c844e019ea58737b695efa3
cuhte3 <farid@getportal.co>
2022-09-29 20:07:12 +0700

    Update WalletItem.swift

--- BDKDemoApp/Models/WalletItem.swift
+++ BDKDemoApp/Models/WalletItem.swift
@@ -17,62 +17,6 @@ struct WalletItem: Identifiable {
 
 extension WalletItem {
     static var mockedBtc: WalletItem {
-        WalletItem(viewModel: WalletItemViewModel.config(coin: .bitcoin()))
-    }
-}
-
-import Combine
-
-class WalletItemViewModel: ObservableObject {
-    let coin: Coin
-    private let balanceAdapter: IBalanceAdapter
-    private var subscriptions = Set<AnyCancellable>()
-    @Injected(Container.marketData) private var marketData
-    
-    var balance: Decimal {
-        balanceAdapter.balance
-    }
-    
-    var balanceString: String {
-        "\(balance)"
-    }
-    
-    var valueString: String {
-        let usdCurrency = FiatCurrency(code: "USD", name: "Dollar", rate: 1)
-        switch coin.type {
-        case .bitcoin:
-            let usd = marketData.fiatCurrencies.first(where: { $0.code == "USD"}) ?? usdCurrency
-            let btcPriceInUsd = marketData.btcTicker?[.usd].price ?? 1
-            return (balance * (btcPriceInUsd/100_000_000)).double.formattedString(.fiat(usd))
-        case .lightningBitcoin:
-            let usd = marketData.fiatCurrencies.first(where: { $0.code == "USD"}) ?? usdCurrency
-            let btcPriceInUsd = marketData.btcTicker?[.usd].price ?? 1
-            return (balance * btcPriceInUsd).double.formattedString(.fiat(usd))
-        case .ethereum, .erc20:
-            return "not implemented"
-        }
-    }
-    
-    init(coin: Coin, balanceAdapter: IBalanceAdapter) {
-        self.coin = coin
-        self.balanceAdapter = balanceAdapter
-        subscribeForUpdates()
-    }
-    
-    private func subscribeForUpdates() {
-        balanceAdapter.balanceUpdated.sink { [weak self] in
-            self?.objectWillChange.send()
-        }
-        .store(in: &subscriptions)
-    }
-}
-
-extension WalletItemViewModel {
-    static func config(coin: Coin) -> WalletItemViewModel {
-        let adapterManager = Container.adapterManager()
-        guard let balanceAdapter = adapterManager.balanceAdapter(for: Wallet(coin: coin, account: Account.mocked)) else {
-            fatalError("Balance adapter for \(coin) is nil")
-        }
-        return WalletItemViewModel(coin: coin, balanceAdapter: balanceAdapter)
+        WalletItem(viewModel: WalletItemViewModel.mocked)
     }
 }
