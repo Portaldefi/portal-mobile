b55937801a2f820cb2ad6ac0e3de1ad00971b3af
cuhte3 <farid@getportal.co>
2022-09-29 20:04:17 +0700

    Create IMarketDataRepository.swift

---
+++ BDKDemoApp/Protocols/IMarketDataRepository.swift
@@ -0,0 +1,17 @@
+//
+//  IMarketDataRepository.swift
+//  BDKDemoApp
+//
+//  Created by farid on 9/29/22.
+//
+
+import Foundation
+import Combine
+
+protocol IMarketDataRepository {
+    var onMarketDataUpdate: PassthroughSubject<Void, Never> { get }
+    var btcTicker: Ticker? { get }
+    var fiatCurrencies: [FiatCurrency] { get }
+    func pause()
+    func resume()
+}
