688306f749244418c11b01aca495911d94fdff36
cuhte3 <farid@getportal.co>
2022-09-29 20:04:11 +0700

    Create BalanceAdapterMocked.swift

---
+++ BDKDemoApp/Models/BalanceAdapterMocked.swift
@@ -0,0 +1,20 @@
+//
+//  BalanceAdapterMocked.swift
+//  BDKDemoApp
+//
+//  Created by farid on 9/29/22.
+//
+
+import Foundation
+import Combine
+
+class BalanceAdapterMocked: IBalanceAdapter {
+    var state: AdapterState = .synced
+    var balance: Decimal = 0.00055
+    var balanceStateUpdated: AnyPublisher<Void, Never> {
+        Just(()).eraseToAnyPublisher()
+    }
+    var balanceUpdated: AnyPublisher<Void, Never> {
+        Just(()).eraseToAnyPublisher()
+    }
+}
