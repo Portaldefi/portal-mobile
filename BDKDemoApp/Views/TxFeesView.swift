b253fefaf0e0fc6ca3091271b92019cb29413e40
cuhte3 <farid@getportal.co>
2022-10-02 18:50:50 +0700

    Create TxFeesView.swift

---
+++ BDKDemoApp/Views/TxFeesView.swift
@@ -0,0 +1,36 @@
+//
+//  TxFeesView.swift
+//  BDKDemoApp
+//
+//  Created by farid on 10/2/22.
+//
+
+import SwiftUI
+import PortalUI
+
+struct TxFeesView: View {
+    let fees: String
+    
+    var body: some View {
+        HStack {
+            Text("Fees")
+                .font(.Main.fixed(.monoBold, size: 14))
+                .foregroundColor(Palette.grayScaleAA)
+            Spacer()
+            HStack(alignment: .firstTextBaseline, spacing: 4) {
+                Text(fees)
+                    .font(.Main.fixed(.monoRegular, size: 16))
+                Text("btc")
+                    .font(.Main.fixed(.monoMedium, size: 12))
+            }
+            .foregroundColor(Palette.grayScaleF4)
+        }
+        .frame(height: 72)
+    }
+}
+
+struct TxFeesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TxFeesView(fees: "0.000000134")
+    }
+}
