2c625758526f42486749b0a4eb9ece89a85ef687
cuhte3 <farid@getportal.co>
2022-10-03 15:01:17 +0700

    Create TxLabel.swift

---
+++ BDKDemoApp/Models/TxLabel.swift
@@ -0,0 +1,13 @@
+//
+//  TxLabel.swift
+//  BDKDemoApp
+//
+//  Created by farid on 10/3/22.
+//
+
+import Foundation
+
+struct TxLable: Identifiable {
+    let id: UUID = UUID()
+    let label: String
+}
