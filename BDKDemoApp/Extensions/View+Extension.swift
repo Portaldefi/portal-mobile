09214b471001671d1bf3f6d59cbb9319cbdeec91
cuhte3 <farid@getportal.co>
2022-10-03 19:18:47 +0700

    Update View+Extension.swift

--- BDKDemoApp/Extensions/View+Extension.swift
+++ BDKDemoApp/Extensions/View+Extension.swift
@@ -25,4 +25,12 @@ extension View {
             self
         }
     }
+    
+    func textFieldAlert(isPresented: Binding<Bool>, content: @escaping () -> TextFieldAlert) -> some View {
+        TextFieldWrapper(
+            isPresented: isPresented,
+            presentingView: self,
+            content: content
+        )
+    }
 }
