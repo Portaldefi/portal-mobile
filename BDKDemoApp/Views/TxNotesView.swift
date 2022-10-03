e552d25814f87cde4bc6d3532081ff5d1795c359
cuhte3 <farid@getportal.co>
2022-10-03 15:00:48 +0700

    Create TxNotesView.swift

---
+++ BDKDemoApp/Views/TxNotesView.swift
@@ -0,0 +1,41 @@
+//
+//  TxNotesView.swift
+//  BDKDemoApp
+//
+//  Created by farid on 10/3/22.
+//
+
+import SwiftUI
+import PortalUI
+
+struct TxNotesView: View {
+    let notes: String
+    
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            HStack(alignment: .firstTextBaseline) {
+                Text("Notes")
+                    .font(.Main.fixed(.monoBold, size: 14))
+                    .foregroundColor(Palette.grayScaleAA)
+                    .frame(width: 50)
+                Spacer()
+                Text(notes)
+                    .font(.Main.fixed(.monoBold, size: 14))
+                    .foregroundColor(Palette.grayScaleF4)
+                    .multilineTextAlignment(.leading)
+            }
+            .padding(.vertical, 16)
+            
+            Asset.chevronRightIcon
+                .foregroundColor(Palette.grayScaleF4)
+                .offset(x: 16)
+        }
+    }
+}
+
+struct TxNotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        TxNotesView(notes: "Design Services. Payment 1/5 and some longer text to test the expandability of the component|")
+            .previewLayout(.sizeThatFits)
+    }
+}
