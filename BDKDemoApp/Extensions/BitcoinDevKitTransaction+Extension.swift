1e9add7de987213dff77c71a78f6f25ea2058006
cuhte3 <farid@getportal.co>
2022-10-04 20:53:42 +0700

    Update BitcoinDevKitTransaction+Extension.swift

--- BDKDemoApp/Extensions/BitcoinDevKitTransaction+Extension.swift
+++ BDKDemoApp/Extensions/BitcoinDevKitTransaction+Extension.swift
@@ -6,6 +6,7 @@
 //
 
 import BitcoinDevKit
+import Foundation
 
 extension BitcoinDevKit.Transaction {
     static var mockedConfirmed: BitcoinDevKit.Transaction {
@@ -18,5 +19,14 @@ extension BitcoinDevKit.Transaction {
         let blockTime = BlockTime(height: 2345912, timestamp: 1662707961)
         
         return BitcoinDevKit.Transaction.confirmed(details: details, confirmation: blockTime)
+    }
+    
+    static func unconfirmedSentTransaction(recipient: String, amount: String, id: String) -> BitcoinDevKit.Transaction {
+        let satAmountDouble = (Double(amount) ?? 0) * 100_000_000
+        let satAmountInt = UInt64(satAmountDouble)
+        let defaults = UserDefaults.standard
+        defaults.set("\(recipient.prefix(4))...\(recipient.suffix(4))", forKey: id + "recipient")
+        let details = TransactionDetails(fee: nil, received: 0, sent: satAmountInt, txid: id)
+        return BitcoinDevKit.Transaction.unconfirmed(details: details)
     }
 }
