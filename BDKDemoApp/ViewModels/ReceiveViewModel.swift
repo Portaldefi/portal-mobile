06a56bbb5a204f128a9ff95dd89dd1097bb79e75
cuhte3 <farid@getportal.co>
2022-10-20 20:12:01 +0700

    Update ReceiveViewModel.swift

--- BDKDemoApp/ViewModels/ReceiveViewModel.swift
+++ BDKDemoApp/ViewModels/ReceiveViewModel.swift
@@ -12,7 +12,11 @@ import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 class ReceiveViewModel: ObservableObject {
-    private let adapter: IDepositAdapter
+    enum RecieveStep {
+        case selectAsset, generateQR
+    }
+    
+    private var adapter: IDepositAdapter?
     private let context = CIContext()
     private let filter = CIFilter.qrCodeGenerator()
     
@@ -23,18 +27,87 @@ class ReceiveViewModel: ObservableObject {
     @Published var editingDescription = false
     
     @Published private(set) var qrCode = UIImage()
+    @Published private(set) var step: RecieveStep = .selectAsset
+    @Published private(set) var walletItems = [WalletItem]()
+    
+    @Published var sharedAddress: IdentifiableString?
+    @Published var selectedItem: WalletItem?
+    
+    private var subscriptions = Set<AnyCancellable>()
     
     var receiveAddress: String {
-        adapter.receiveAddress
+        adapter?.receiveAddress ?? String()
     }
     
-    init(depositAdapter: IDepositAdapter) {
-        adapter = depositAdapter
-        generateQRCode()
+    init(items: [WalletItem], selectedItem: WalletItem?) {
+        self.walletItems = items
+        self.selectedItem = selectedItem
+        
+        Publishers.CombineLatest($amount, $description)
+            .flatMap { _ in Just(()) }
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] _ in
+                self.generateQRCode()
+            }
+            .store(in: &subscriptions)
+        
+        $selectedItem
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] item in
+                self.updateAdapter()
+
+                if item != nil {
+                    withAnimation {
+                        self.generateQRCode()
+                        self.step = .generateQR
+                    }
+                } else {
+                    withAnimation {
+                        self.step = .selectAsset
+                    }
+                }
+        }
+        .store(in: &subscriptions)
+    }
+    
+    private func updateAdapter() {
+        guard let selectedItem = selectedItem else { return }
+        
+        let adapterManager: IAdapterManager = Container.adapterManager()
+        let walletManager: IWalletManager = Container.walletManager()
+                
+        guard
+            let wallet = walletManager.activeWallets.first(where: { $0.coin == selectedItem.viewModel.coin }),
+            let depositAdapter = adapterManager.depositAdapter(for: wallet)
+        else {
+            fatalError("coudn't fetch dependencies")
+        }
+        
+        self.adapter = depositAdapter
     }
     
     private func generateQRCode() {
-        let qrCodeString = "bitcoin:\(adapter.receiveAddress)"
+        guard let adapter = adapter else { return }
+        
+        var qrCodeString = "bitcoin:\(adapter.receiveAddress)"
+        
+        var components = URLComponents()
+        components.queryItems = []
+        
+        if !amount.isEmpty {
+            components.queryItems?.append(URLQueryItem(name: "amount", value: amount))
+        }
+
+        if !description.isEmpty {
+            components.queryItems?.append(URLQueryItem(name: "message", value: description))
+        }
+                
+        if let parameters = components.string {
+            qrCodeString += parameters
+        }
+        
+        print("QR CODE STRING: \(qrCodeString)")
+        
         let data = Data(qrCodeString.utf8)
         filter.setValue(data, forKey: "inputMessage")
 
@@ -50,26 +123,23 @@ class ReceiveViewModel: ObservableObject {
     }
     
     func copyToClipboard() {
+        guard let adapter = adapter else { return }
+
         UIPasteboard.general.string = adapter.receiveAddress
+    }
+    
+    func share() {
+        guard let adapter = adapter else { return }
+        sharedAddress = IdentifiableString(text: "\(adapter.receiveAddress)\n\nThis is a bitcoin network address. Only send BTC to this address. Do not send lightning network assets to his address.")
     }
 }
 
 extension ReceiveViewModel {
-    static func config(coin: Coin) -> ReceiveViewModel {
-        let adapterManager: IAdapterManager = Container.adapterManager()
-        let walletManager: IWalletManager = Container.walletManager()
-                
-        guard
-            let wallet = walletManager.activeWallets.first(where: { $0.coin == coin }),
-            let depositAdapter = adapterManager.depositAdapter(for: wallet)
-        else {
-            fatalError("coudn't fetch dependencies")
-        }
-        
-        return ReceiveViewModel(depositAdapter: depositAdapter)
+    static func config(items: [WalletItem], selectedItem: WalletItem?) -> ReceiveViewModel {
+        ReceiveViewModel(items: items, selectedItem: selectedItem)
     }
     
     static var mocked: ReceiveViewModel {
-        ReceiveViewModel(depositAdapter: DepositAdapterMocked())
+        ReceiveViewModel(items: [WalletItem.mockedBtc], selectedItem: nil)
     }
 }
