443859a2db6217aed7c0ddcc78f6aa55454fe5ee
cuhte3 <farid@getportal.co>
2022-11-03 19:47:33 +0700

    Update BitcoinDevKitTransaction+Extension.swift
    enum TxType addded
    note, labels wrappers

--- BDKDemoApp/Extensions/BitcoinDevKitTransaction+Extension.swift
+++ BDKDemoApp/Extensions/BitcoinDevKitTransaction+Extension.swift
@@ -8,6 +8,21 @@
 import BitcoinDevKit
 import Foundation
 
+enum TxType: Equatable {
+    case sent, recieved, swapped(for: Coin)
+    
+    var description: String {
+        switch self {
+        case .sent:
+            return "Sent"
+        case .recieved:
+            return "Recieved"
+        case .swapped(for: let coin):
+            return "Swapped for \(coin.code)"
+        }
+    }
+}
+
 extension BitcoinDevKit.TransactionDetails {
     static var mockedConfirmed: BitcoinDevKit.TransactionDetails {
         let blockTime = BlockTime(height: 2345912, timestamp: 1662707961)
@@ -26,5 +41,33 @@ extension BitcoinDevKit.TransactionDetails {
         let defaults = UserDefaults.standard
         defaults.set("\(recipient.prefix(4))...\(recipient.suffix(4))", forKey: id + "recipient")
         return TransactionDetails(fee: nil, received: 0, sent: satAmountInt, txid: id, confirmationTime: nil)
+    }
+    
+    var type: TxType {
+        if self.sent > 0 {
+            return .sent
+        } else {
+            return .recieved
+        }
+    }
+    
+    var confirmationTimeString: String? {
+        guard let confirmationTime = self.confirmationTime else { return nil }
+        return Date(timeIntervalSince1970: TimeInterval(confirmationTime.timestamp)).formatted()
+    }
+    
+    var notes: String?  {
+        UserDefaults.standard.string(forKey: self.txid + "notes")
+    }
+    
+    var labels: [TxLable] {
+        guard let tags = UserDefaults.standard.object(forKey: self.txid + "labels") as? [String] else {
+            return []
+        }
+        return tags.map{ TxLable(label: $0 )}
+    }
+    
+    var value: String {
+        self.sent > 0 ? "\(Double(self.sent)/100_000_000)" : "\(Double(self.received)/100_000_000)"
     }
 }
