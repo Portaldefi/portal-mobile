412a0dfcd97d5abfa80e2d5d1b2aa348f6980ba3
cuhte3 <farid@getportal.co>
2022-12-13 19:55:13 +0700

    Create NoAccountViewConfigurator.swift

---
+++ Portal/Views/Onboarding/NoAccount/NoAccountViewConfigurator.swift
@@ -0,0 +1,27 @@
+//
+//  NoAccountViewConfigurator.swift
+//  Portal
+//
+//  Created by farid on 12/13/22.
+//
+
+import SwiftUI
+
+struct NoAccountViewConfigurator: NavigationConfigurator {
+    func configure(_ screen: Screen) -> ViewElement? {
+        switch screen {
+        case .createAccount:
+            return ViewElement(
+                id: screen.id,
+                wrappedElement: AnyView(CreateAccountView())
+            )
+        case .restoreAccount:
+            return ViewElement(
+                id: screen.id,
+                wrappedElement: AnyView(RestoreAccountView())
+            )
+        default:
+            fatalError("unsupported navigation case")
+        }
+    }
+}
