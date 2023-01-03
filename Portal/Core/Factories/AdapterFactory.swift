6396096343b35d940cefee40e93e02e71c01e8c1
cuhte3 <farid@getportal.co>
2023-01-03 21:57:57 +0700

    Update AdapterFactory.swift

--- Portal/Core/Factories/AdapterFactory.swift
+++ Portal/Core/Factories/AdapterFactory.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 class AdapterFactory: IAdapterFactory {
+    private let appConfigProvider: IAppConfigProvider
+    private let ethereumKitManager: EthereumKitManager
+
+    init(appConfigProvider: IAppConfigProvider, ethereumKitManager: EthereumKitManager) {
+        self.appConfigProvider = appConfigProvider
+        self.ethereumKitManager = ethereumKitManager
+    }
+    
     func adapter(wallet: Wallet) -> IAdapter? {
         switch wallet.coin.type {
         case .bitcoin:
@@ -18,7 +26,11 @@ class AdapterFactory: IAdapterFactory {
                 fatalError(error.localizedDescription)
             }
         case .ethereum:
-            return EthereumKitManager.shared.adapter
+            if let ethKit = try? ethereumKitManager.kit(account: wallet.account) {
+                return EthereumAdapter(evmKit: ethKit, signer: ethereumKitManager.signer)
+            } else {
+                return nil
+            }
         default:
             return nil
         }
