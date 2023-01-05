7978e70d8a86e5172952ae66bad96631edd289c4
cuhte3 <farid@getportal.co>
2023-01-05 21:28:35 +0700

    Update BalanceAdapterMocked.swift

--- Portal/Models/BalanceAdapterMocked.swift
+++ Portal/Models/BalanceAdapterMocked.swift
@@ -29,6 +29,38 @@ class SendAdapterMocked: ISendAdapter {
     }
 }
 
+class SendAssetMockedService: ISendAssetService {
+    var balance: Decimal = 1.1
+    var spendable: Decimal = 1.1
+    var fee: Decimal = 0.0001
+    var amount: CurrentValueSubject<Decimal, Never> = CurrentValueSubject<Decimal, Never>(0.001)
+    var feeRate: CurrentValueSubject<Int, Never> = CurrentValueSubject<Int, Never>(1)
+    var receiverAddress: CurrentValueSubject<String, Never> = CurrentValueSubject<String, Never>("tb1q3ds30e5p59x9ryee4e2kxz9vxg5ur0tjsv0ug3")
+    var feeRateProvider: IFeeRateProvider = MockeFeeRateProvider()
+    
+    init() {
+        
+    }
+    
+    func validateAddress() throws {
+        
+    }
+    
+    func send() -> Future<Void, Error> {
+        Future { promise in
+            promise(.success(()))
+        }
+    }
+}
+
+class MockeFeeRateProvider: IFeeRateProvider {
+    var recommendedFeeRate: Future<Int, Never> {
+        Future { promise in
+            promise(.success(10000))
+        }
+    }
+}
+
 class DepositAdapterMocked: IDepositAdapter {
     var receiveAddress: String {
         "tb1q3ds30e5p59x9ryee4e2kxz9vxg5ur0tjsv0ug3"
