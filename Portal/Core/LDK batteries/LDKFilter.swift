057abc7315a1b47f433e229a7185624c4fe0a71f
cuhte3 <farid@getportal.co>
2023-01-27 19:00:00 +0700

    Update LDKFilter.swift

--- Portal/Core/LDK batteries/LDKFilter.swift
+++ Portal/Core/LDK batteries/LDKFilter.swift
@@ -8,29 +8,17 @@
 import LightningDevKit
 
 class LDKFilter: Filter {
-//    override func register_tx(txid: [UInt8]?, script_pubkey: [UInt8]) {
-//        // watch this transaction on-chain
-//    }
-//
-//    override func register_output(output: WatchedOutput) -> Option_C2Tuple_usizeTransactionZZ {
-////        let scriptPubkeyBytes = output.get_script_pubkey()
-////        let outpoint = output.get_outpoint()!
-////        let txid = outpoint.get_txid()
-////        let outputIndex = outpoint.get_index()
-////
-////        // watch for any transactions that spend this output on-chain
-////
-////        let blockHashBytes = output.get_block_hash()
-////        // if block hash bytes are not null, return any transaction spending the output that is found in the corresponding block along with its index
-//
-//        return Option_C2Tuple_usizeTransactionZZ.none()
-//    }
-    
     override func registerTx(txid: [UInt8]?, scriptPubkey: [UInt8]) {
-        
+    
     }
     
     override func registerOutput(output: Bindings.WatchedOutput) {
-        
+        let scriptPubkeyBytes = output.getScriptPubkey()
+        let outpoint = output.getOutpoint()
+        let txid = outpoint.getTxid()
+        let outputIndex = outpoint.getIndex()
+        // watch for any transactions that spend this output on-chain
+        let blockHashBytes = output.getBlockHash()
+        // if block hash bytes are not null, return any transaction spending the output that is found in the corresponding block along with its index
     }
 }
