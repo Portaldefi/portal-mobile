33fb2a94d381bebf67362d7d0327bca39241d464
cuhte3 <farid@getportal.co>
2023-02-02 18:52:53 +0700

    Create Peer.swift

---
+++ Portal/Models/Peer.swift
@@ -0,0 +1,35 @@
+//
+//  Peer.swift
+//  Portal
+//
+//  Created by farid on 1/5/23.
+//
+
+
+import Foundation
+
+struct Peer: Identifiable, Codable, Equatable {
+    let id: UUID
+    let peerPubKey: String
+    let name: String
+    let connectionInformation: PeerConnectionInformation
+        
+    internal init(id: UUID = UUID(), peerPubKey: String, name: String, connectionInformation: PeerConnectionInformation) {
+        self.id = id
+        self.peerPubKey = peerPubKey
+        self.name = name
+        self.connectionInformation = connectionInformation
+    }
+    
+    static func == (lhs: Peer, rhs: Peer) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+// MARK: Helper Models
+extension Peer {
+    struct PeerConnectionInformation: Codable {
+        let hostname: String
+        let port: UInt16
+    }
+}
