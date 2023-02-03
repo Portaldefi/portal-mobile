2ef3c129821f18621bee8d93e2800c2c38bc255e
cuhte3 <farid@getportal.co>
2023-02-03 21:28:29 +0700

    Update WalletItemView.swift
    chain description labels removed

--- Portal/Views/WalletItemView.swift
+++ Portal/Views/WalletItemView.swift
@@ -32,19 +32,6 @@ struct WalletItemView: View {
                             .foregroundColor(.white)
                             .frame(height: 16)
                     }
-                    HStack(spacing: 4) {
-                        Text("on")
-                            .font(.Main.fixed(.monoMedium, size: 14))
-                            .foregroundColor(Palette.grayScale6A)
-                            .frame(height: 17)
-                        viewModel.coin.chainIcon.resizable()
-                            .frame(width: 12, height: 12)
-                            .foregroundColor(Palette.grayScale8A)
-                        Text(viewModel.coin.description)
-                            .font(.Main.fixed(.monoMedium, size: 14))
-                            .foregroundColor(Palette.grayScale6A)
-                            .frame(height: 17)
-                    }
                 }
             }
             Spacer()
@@ -76,6 +63,7 @@ struct WalletItemView: View {
                 }
             }
         }
+        .padding(.vertical, 12)
         .frame(height: 70)
     }
 }
