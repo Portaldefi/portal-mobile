700b4947a851b47f05e8ae58f8eea51ffeec10db
cuhte3 <farid@getportal.co>
2023-02-07 18:59:12 +0700

    Create QRReaderRootView.swift

---
+++ Portal/Views/QRCodeReader/QRReaderRootView.swift
@@ -0,0 +1,24 @@
+//
+//  QRReaderRootView.swift
+//  Portal
+//
+//  Created by farid on 2/7/23.
+//
+
+import SwiftUI
+import Factory
+
+struct QRCodeReaderRootView: View {
+    private let navigationStack: NavigationStackView<QRCodeReaderView>
+    
+    init(config: QRScannerConfig, completion: @escaping (QRCodeItem) ->() = { _ in }) {
+        navigationStack = NavigationStackView<QRCodeReaderView>(
+            configurator: SendViewNavigationConfig(),
+            rootView: QRCodeReaderView(config: config) { completion($0) }
+        )
+    }
+    
+    var body: some View {
+        navigationStack.zIndex(1).navigationBarBackButtonHidden()
+    }
+}
