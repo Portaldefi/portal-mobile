77fef74fb6591e8a991300138cbf6156ba5171ac
cuhte3 <farid@getportal.co>
2023-02-07 19:00:16 +0700

    Update ReceiveViewNavigationConfig.swift
    receiveGenerateQRCode added

--- Portal/Views/Receive/ReceiveViewNavigationConfig.swift
+++ Portal/Views/Receive/ReceiveViewNavigationConfig.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ReceiveViewNavigationConfig: NavigationConfigurator {
     func configure(_ screen: Screen) -> ViewElement? {
         switch screen {
+        case .receiveGenerateQRCode(let viewModel):
+            return ViewElement(
+                id: screen.id,
+                wrappedElement: AnyView(QRCodeGeneratorView(rootView: false, viewModel: viewModel))
+            )
         default:
             fatalError("unsupported navigation case")
         }
