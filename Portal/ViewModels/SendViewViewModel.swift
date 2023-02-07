072c3bdf24b3273861d64acd2ba6d14023f76dfd
cuhte3 <farid@getportal.co>
2023-02-07 16:18:40 +0700

    Update SendViewViewModel.swift
    SendViewViewModel refactor:
    txSent, editingAmount, sendMax, actionButtonEnabled, actionButtonTitle properties removed
    unwrapAuthenticationError, clearAmount, clearRecipient functions added
    goBack, onActionButtonPressed functions removed

--- Portal/ViewModels/SendViewViewModel.swift
+++ Portal/ViewModels/SendViewViewModel.swift
@@ -18,29 +18,22 @@ class SendViewViewModel: ObservableObject {
     private(set) var walletItems: [WalletItem]  = []
     
     @Published var receiverAddress = String()
-    @Published var txSent = false
     @Published var coin: Coin?
     @Published var qrCodeItem: QRCodeItem?
     @Published var clipboardIsEmpty = false
-    @Published var editingAmount = false
     @Published var feeRate: TxFees = .normal
     @Published var amountIsValid: Bool = true
-    @Published var sendMax: Bool = false
     
     @Published private(set) var balanceString = String()
     @Published private(set) var valueString = String()
     @Published private(set) var useAllFundsEnabled = true
-    @Published private(set) var recipientAddressIsValid = true
-    @Published private(set) var recomendedFees: RecomendedFees?
-    @Published private(set) var sendError: Error?
-    @Published private(set) var step: SendStep = .selectAsset
-    @Published private(set) var publishedTxId: String?
+    
     @Published private(set) var unconfirmedTx: TransactionRecord?
+    @Published private(set) var recomendedFees: RecomendedFees?
     @Published private(set) var exchanger: Exchanger?
+    @Published private(set) var sendError: Error?
         
-    @ObservedObject var viewState = Container.viewState()
-    @ObservedObject private var account: AccountViewModel = Container.accountViewModel()
-
+    @Injected(Container.accountViewModel) private var account
     @Injected(Container.marketData) private var marketData
     @LazyInjected(Container.biometricAuthentification) private var biometrics
         
@@ -51,51 +44,6 @@ class SendViewViewModel: ObservableObject {
             return ((sendService.fee.double)/100_000_000).formattedString(.coin(coin), decimals: 8)
         case .ethereum, .erc20:
             return recomendedFees.fee(feeRate).double.formattedString(.coin(coin), decimals: 8)
-        }
-    }
-    
-    var actionButtonEnabled: Bool {
-        guard let exchanger = exchanger else { return false }
-        
-        switch step {
-        case .recipient:
-            return !receiverAddress.isEmpty
-        case .amount:
-            return amountIsValid && exchanger.baseAmountDecimal > 0
-        case .review:
-            return amountIsValid
-        case .signing, .sent:
-            return false
-        case .selectAsset:
-            return false
-        }
-    }
-    
-    var actionButtonTitle: String {
-        switch step {
-        case .recipient, .amount:
-            return "Continue"
-        case .review:
-            return "Send"
-        case .signing, .sent:
-            return String()
-        case .selectAsset:
-            return String()
-        }
-    }
-    
-    var title: String {
-        switch step {
-        case .recipient, .amount:
-            return "Send"
-        case .review:
-            return "Review Transaction"
-        case .signing:
-            return "Singing..."
-        case .sent:
-            return "Signed!"
-        case .selectAsset:
-            return "Send"
         }
     }
     
@@ -117,49 +65,15 @@ class SendViewViewModel: ObservableObject {
                 
                 self.syncSendService(coin: coin)
                 self.syncExchanger(coin: coin)
-                
-                withAnimation {
-                    self.step = .recipient
-                }
-            }
-            .store(in: &subscriptions)
-        
-        $qrCodeItem
-            .sink { [unowned self] item in
-                guard let item = item else { return }
-                switch item.type {
-                case .bip21(let address, let amount, _):
-                    self.coin = walletItems.first?.viewModel.coin
-                    self.receiverAddress = address
-                    guard let amount = amount else {
-                        step = .amount
-                        return
-                    }
-                    self.exchanger?.amount.string = amount
-                    self.step = .review
-                case .eth(let address, let amount, _):
-                    self.coin = walletItems.last?.viewModel.coin
-                    self.receiverAddress = address
-                    guard let amount = amount else {
-                        step = .amount
-                        return
-                    }
-                    self.exchanger?.amount.string = amount
-                    self.step = .review
-                default:
-                    break
-                }
-                
             }
             .store(in: &subscriptions)
         
         $receiverAddress
             .sink { [unowned self] address in
                 self.sendService?.receiverAddress.send(address)
-                guard sendError != nil, recipientAddressIsValid != true else { return }
+                guard sendError != nil else { return }
                 withAnimation {
                     self.sendError = nil
-                    self.recipientAddressIsValid = true
                 }
             }
             .store(in: &subscriptions)
@@ -189,12 +103,6 @@ class SendViewViewModel: ObservableObject {
                 }
             }
             .store(in: &subscriptions)
-        
-        $step
-            .filter{ $0 == .sent }
-            .flatMap{ _ in Just(true) }
-            .delay(for: .seconds(1), scheduler: RunLoop.main)
-            .assign(to: &$txSent)
         
         $feeRate
             .sink { [unowned self] rate in
@@ -285,30 +193,97 @@ class SendViewViewModel: ObservableObject {
         }
         .store(in: &subscriptions)
     }
+        
+    private func unwrapAuthenticationError(error: LAError) -> String {
+        switch error {
+        case LAError.appCancel:
+            // The app canceled authentication by
+            // invalidating the LAContext
+            return "The app canceled authentication by invalidating the LAContext"
+        case LAError.authenticationFailed:
+            // The user did not provide
+            // valid credentials
+            return "The user did not provide valid credentials"
+        case LAError.invalidContext:
+            // The LAContext was invalid
+            return "The LAContext was invalid"
+        case LAError.notInteractive:
+            // Interaction was not allowed so the
+            // authentication failed
+            return "Interaction was not allowed so the authentication failed"
+        case LAError.passcodeNotSet:
+            // The user has not set a passcode
+            // on this device
+            return "The user has not set a passcode on this device"
+        case LAError.systemCancel:
+            // The system canceled authentication,
+            // for example to show another app
+            return "The system canceled authentication, for example to show another app"
+        case LAError.userCancel:
+            // The user canceled the
+            // authentication dialog
+            return "The user canceled the authentication dialog"
+        case LAError.userFallback:
+            // The user selected to use a fallback
+            // authentication method
+            return "The user selected to use a fallback authentication method"
+        case LAError.biometryLockout:
+            // Too many failed attempts locked
+            // biometric authentication
+            return "Too many failed attempts locked biometric authentication"
+        case LAError.biometryNotAvailable:
+            // The user's device does not support
+            // biometric authentication
+            return "The user's device does not support biometric authentication"
+        case LAError.biometryNotEnrolled:
+            // The user has not configured
+            // biometric authentication
+            return "The user has not configured biometric authentication"
+        default:
+            return "Unknown authentification error"
+        }
+    }
+        
+    func validateReceiverAddress() {
+        do {
+            try sendService?.validateAddress()
+        } catch {
+            print("validation error: \(error)")
+            sendError = SendFlowError.addressIsntValid
+        }
+    }
     
-    func send(max: Bool) {
-        if max {
+    func authenticateUser(completion: @escaping (Bool) -> Void) {
+        biometrics.authenticateUser { [unowned self] success, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.sendError = SendFlowError.error(self.unwrapAuthenticationError(error: error))
+                }
+            }
+            completion(success)
+        }
+    }
+    
+    func send(_ completionHandler: @escaping (Bool) -> ()) {
+        if useAllFundsEnabled {
             sendService?
                 .sendMax()
                 .receive(on: RunLoop.main)
                 .sink(receiveCompletion: { [weak self] completion in
                     guard let self = self else { return }
-                    
+
                     if case let .failure(error) = completion {
                         withAnimation {
-                            self.step = .review
                             self.sendError = error
                         }
+                        completionHandler(false)
                     }
                 }, receiveValue: { [weak self] transaction in
                     guard let self = self else { return }
 
-                    self.publishedTxId = transaction.id
                     self.unconfirmedTx = transaction
-                    
-                    withAnimation {
-                        self.step = .sent
-                    }
+
+                    completionHandler(true)
                 })
                 .store(in: &subscriptions)
         } else {
@@ -317,164 +292,34 @@ class SendViewViewModel: ObservableObject {
                 .receive(on: RunLoop.main)
                 .sink(receiveCompletion: { [weak self] completion in
                     guard let self = self else { return }
-                    
+
                     if case let .failure(error) = completion {
                         withAnimation {
-                            self.step = .review
                             self.sendError = error
                         }
+                        completionHandler(false)
                     }
                 }, receiveValue: { [weak self] transaction in
                     guard let self = self else { return }
 
-                    self.publishedTxId = transaction.id
                     self.unconfirmedTx = transaction
-                    
-                    withAnimation {
-                        self.step = .sent
-                    }
+
+                    completionHandler(true)
                 })
                 .store(in: &subscriptions)
         }
     }
     
-    func authenticateUser(_ completion: @escaping (Bool) -> ()) {
-        biometrics.authenticateUser { success, error in
-            guard let error = error else {
-                return completion(success)
-            }
-
-            switch error {
-            case LAError.appCancel:
-                // The app canceled authentication by
-                // invalidating the LAContext
-                completion(success)
-            case LAError.authenticationFailed:
-                // The user did not provide
-                // valid credentials
-                completion(success)
-            case LAError.invalidContext:
-                // The LAContext was invalid
-                completion(success)
-            case LAError.notInteractive:
-                // Interaction was not allowed so the
-                // authentication failed
-                completion(success)
-            case LAError.passcodeNotSet:
-                // The user has not set a passcode
-                // on this device
-                completion(success)
-            case LAError.systemCancel:
-                // The system canceled authentication,
-                // for example to show another app
-                completion(success)
-            case LAError.userCancel:
-                // The user canceled the
-                // authentication dialog
-                completion(false)
-            case LAError.userFallback:
-                // The user selected to use a fallback
-                // authentication method
-                completion(success)
-            case LAError.biometryLockout:
-                // Too many failed attempts locked
-                // biometric authentication
-                completion(success)
-            case LAError.biometryNotAvailable:
-                // The user's device does not support
-                // biometric authentication
-                completion(true)
-            case LAError.biometryNotEnrolled:
-                // The user has not configured
-                // biometric authentication
-                completion(true)
-            default:
-                completion(success)
-            }
-        }
-    }
-    
-    func goBack() -> Bool {
-        var shouldCloseSendFlow = false
+    func clearRecipient() {
+        coin = nil
+        receiverAddress = String()
         sendError = nil
-        
-        switch step {
-        case .recipient:
-            coin = nil
-            receiverAddress = String()
-            recipientAddressIsValid = true
-            sendError = nil
-            step = .selectAsset
-        case .amount:
-            exchanger?.amount.string = String()
-            useAllFundsEnabled = true
-            sendError = nil
-            step = .recipient
-        case .review:
-            if viewState.showQRCodeScannerFromTabBar {
-                shouldCloseSendFlow = true
-            } else {
-                step = .amount
-            }
-        case .signing:
-            step = .review
-        case .sent:
-            step = .recipient
-        case .selectAsset:
-            shouldCloseSendFlow = true
-        }
-        
-        return shouldCloseSendFlow
     }
     
-    func toReview() {
-        step = .review
-    }
-    
-    func onActionButtonPressed() {
+    func clearAmount() {
+        exchanger?.amount.string = String()
+        useAllFundsEnabled = true
         sendError = nil
-        
-        switch step {
-        case .recipient:
-            do {
-                try sendService?.validateAddress()
-                step = .amount
-            } catch {
-                print("validation error: \(error)")
-                recipientAddressIsValid = false
-                sendError = SendFlowError.addressIsntValid
-            }
-        case .amount:
-            step = .review
-        case .review:
-            biometrics.authenticateUser { [weak self] success, error in
-                guard let self = self else { return }
-                
-                if let error = error {
-                    DispatchQueue.main.sync {
-                        withAnimation {
-                            self.sendError = SendFlowError.error(error.errorUserInfo.description)
-                        }
-                    }
-                }
-                
-                guard success else { return }
-                    
-                DispatchQueue.main.sync {
-                    withAnimation {
-                        self.step = .signing
-                    }
-                }
-                
-                self.send(max: !self.useAllFundsEnabled)
-            }
-        case .signing:
-            break
-        case .sent:
-            break
-        case .selectAsset:
-            break
-        }
     }
         
     func pasteFromClipboard() {
@@ -485,11 +330,7 @@ class SendViewViewModel: ObservableObject {
             clipboardIsEmpty.toggle()
         }
     }
-    
-    func openScanner() {
-        viewState.showInContextScanner = true
-    }
-    
+        
     func useAllFunds() {
         guard let sendService = sendService, let exchanger = exchanger else { return }
         let spendable = sendService.spendable
