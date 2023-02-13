e8e2595b86f7e11e6dc9d9a3241a3d84138d1dc0
cuhte3 <farid@getportal.co>
2023-02-13 23:00:07 +0700

    Update Portal+Injection.swift

--- Portal/Portal+Injection.swift
+++ Portal/Portal+Injection.swift
@@ -57,7 +57,7 @@ extension SharedContainer {
     }
     
     static let lightningKitManager = Factory<LightningKitManager>(scope: .singleton) {
-        LightningKitManager(connectionType: .regtest(BitcoinCoreRpcConfig(username: "polaruser", password: "polarpass", port: 18443, host: "localhost")))
+        LightningKitManager(connectionType: .testnet(.blockStream))
     }
     
     static let sendViewModel = Factory<SendViewViewModel>(scope: .cached) {
