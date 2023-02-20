d7fae24a4e4a5c58bb6a716dbd0d9c25c9832eb3
cuhte3 <farid@getportal.co>
2023-02-20 22:09:06 +0700

    BTCQRCodeAddressType added

---
+++ Portal/Models/BTCQRCodeAddressType.swift
@@ -0,0 +1,34 @@
+//
+//  BTCQRCodeAddressType.swift
+//  Portal
+//
+//  Created by farid on 2/20/23.
+//
+
+import Foundation
+
+enum BTCQRCodeAddressType: CaseIterable {
+    case lightning, onChain, unified
+    
+    var title: String {
+        switch self {
+        case .lightning:
+            return "Lightnig"
+        case .onChain:
+            return "On Chain"
+        case .unified:
+            return "Unified"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .lightning:
+            return "Instant, with minimal fees. But not all services support it."
+        case .onChain:
+            return "Slower. Higher fees. Most services support it."
+        case .unified:
+            return "An unified QR for both Lightning & On Chain. Most services don’t support it, yet."
+        }
+    }
+}
