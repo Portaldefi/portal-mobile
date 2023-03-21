b99b3649d2727ba2f75bc55612b511bbad0712cd
cuhte3 <farid@getportal.co>
2023-03-21 20:43:51 +0700

    Update ILightningKitManager.swift
    api removed:
    func requestChannelOpen(_ pubKeyHex: String, channelValue: UInt64, reserveAmount: UInt64) async throws -> String
    api added:
    func decode(invoice: String) throws -> Invoice?

--- Portal/Protocols/ILightningKitManager.swift
+++ Portal/Protocols/ILightningKitManager.swift
@@ -6,11 +6,12 @@
 //
 
 import Combine
+import LightningDevKit
 
 protocol ILightningKitManager {
     var activePeersPublisher: AnyPublisher<[String], Never> { get }
     func start() async throws
     func connectPeer(_ peer: Peer) async throws
-    func requestChannelOpen(_ pubKeyHex: String, channelValue: UInt64, reserveAmount: UInt64) async throws -> String
     func createInvoice(amount: String, description: String) async -> String?
+    func decode(invoice: String) throws -> Invoice?
 }
