4439f3348f29b961dec2d467a77fdf44f8f36d15
cuhte3 <farid@getportal.co>
2023-03-27 21:17:09 +0700

    Update MockedLightningKitManager.swift

--- Portal/Models/MockedLightningKitManager.swift
+++ Portal/Models/MockedLightningKitManager.swift
@@ -9,6 +9,16 @@ import Combine
 import LightningDevKit
 
 class MockedLightningKitManager: ILightningKitManager {
+    func pay(invoice: String) -> Future<TransactionRecord, Error> {
+        Future { promise in
+            promise(.success(.mocked))
+        }
+    }
+    
+    var channelBalance: Decimal {
+        Decimal(10000)
+    }
+    
     func decode(invoice: String) throws -> Invoice? {
         Invoice.from_str(s: "lntb255m1p3l3qgadqqnp4qffgdax9g9ux3496d809u6le05nffsccvyuhdyvu5uumvyt7j5twkpp55eham28a4cnwz3epal2geeceskmjs6pxph987shgj3ydnv0nnqvssp5u8meh0nx9jaz68n97h3c22vxvmla2yynjgtcccpu5f4pjt7u7eps9qyysgqcqpcxqzlgsjl4nkpvgu4x54unwafr3s0h2mjtakw9cdklsa28qqdhmsxuqphhytyxlzwfx08nuwez5qvrvcfvkdgwdurtn67cr4l9wkdd6yu8rqgp88xwlu").getValue()
     }
