9f3419b70f1f631eb643cb5430448bff34bbff58
cuhte3 <farid@getportal.co>
2023-03-27 21:16:58 +0700

    Update ILightningKitManager.swift
    added api:
    
    var channelBalance: Decimal
    func pay(invoice: String) -> Future<TransactionRecord, Error>

--- Portal/Protocols/ILightningKitManager.swift
+++ Portal/Protocols/ILightningKitManager.swift
@@ -9,9 +9,11 @@ import Combine
 import LightningDevKit
 
 protocol ILightningKitManager {
+    var channelBalance: Decimal { get }
     var activePeersPublisher: AnyPublisher<[String], Never> { get }
     func start() async throws
     func connectPeer(_ peer: Peer) async throws
     func createInvoice(amount: String, description: String) async -> String?
     func decode(invoice: String) throws -> Invoice?
+    func pay(invoice: String) -> Future<TransactionRecord, Error>
 }
