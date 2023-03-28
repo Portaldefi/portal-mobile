4256742823d360cf7fcc92abf1681bf2c817334a
cuhte3 <farid@getportal.co>
2023-03-28 21:04:21 +0700

    Update PendingEventTracker.swift
    new api:
    
    private lazy var eventPublisher: AsyncStream<Event> is represent an asynchronous stream of node events
    
    func subscribe() -> AsyncStream<Event> added

--- Lightning/Sources/Lightning/Utils/PendingEventTracker.swift
+++ Lightning/Sources/Lightning/Utils/PendingEventTracker.swift
@@ -11,6 +11,17 @@ import LightningDevKit
 actor PendingEventTracker {
     private(set) var pendingManagerEvents: [Event] = []
     private(set) var continuations: [CheckedContinuation<Void, Never>] = []
+    private(set) var eventContinuations: [AsyncStream<Event>.Continuation] = []
+    //Async publisher will emit events stream down to subscribers
+    private lazy var eventPublisher: AsyncStream<Event> = {
+        AsyncStream(Event.self) { [unowned self] continuation in
+            self.addEventContinuation(continuation)
+        }
+    }()
+    
+    private func addEventContinuation(_ continuation: AsyncStream<Event>.Continuation) {
+        self.eventContinuations.append(continuation)
+    }
     
     private func triggerContinuations(){
         let continuations = self.continuations
@@ -23,11 +34,19 @@ actor PendingEventTracker {
     func addEvent(event: Event) {
         self.pendingManagerEvents.append(event)
         self.triggerContinuations()
+        for continuation in eventContinuations {
+            continuation.yield(event)
+        }
     }
     
     func addEvents(events: [Event]) {
         self.pendingManagerEvents.append(contentsOf: events)
         self.triggerContinuations()
+        for event in events {
+            for continuation in eventContinuations {
+                continuation.yield(event)
+            }
+        }
     }
     
     func getCount() -> Int {
@@ -48,5 +67,9 @@ actor PendingEventTracker {
         await withCheckedContinuation({ continuation in
             self.continuations.append(continuation)
         })
+    }
+    
+    func subscribe() -> AsyncStream<Event> {
+        eventPublisher
     }
 }
