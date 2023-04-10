650f11ccbb503398e8a4d394179bc42d876508b9
cuhte3 <farid@getportal.co>
2023-04-10 21:07:01 +0700

    Update ILightningKitManager.swift
    protocol extension
    
    added:
    var allChannels: [ChannelDetails] { get }
    var usableChannels: [ChannelDetails] { get }
    var transactionsPublisher: AnyPublisher<[TransactionRecord], Never> { get }
    func openChannel(peer: Peer) async throws
    func disconnectPeer(_ peer: Peer) throws

--- Portal/Protocols/ILightningKitManager.swift
+++ Portal/Protocols/ILightningKitManager.swift
@@ -9,10 +9,15 @@ import Combine
 import LightningDevKit
 
 protocol ILightningKitManager {
+    var allChannels: [ChannelDetails] { get }
+    var usableChannels: [ChannelDetails] { get }
+    var transactionsPublisher: AnyPublisher<[TransactionRecord], Never> { get }
     var channelBalance: Decimal { get }
     var activePeersPublisher: AnyPublisher<[String], Never> { get }
     func start() async throws
+    func openChannel(peer: Peer) async throws
     func connectPeer(_ peer: Peer) async throws
+    func disconnectPeer(_ peer: Peer) throws
     func createInvoice(amount: String, description: String) async -> String?
     func decode(invoice: String) throws -> Invoice?
     func pay(invoice: String) -> Future<TransactionRecord, Error>
