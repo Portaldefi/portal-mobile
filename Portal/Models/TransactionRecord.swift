db469075ba2f93739e1ce394538fcf58d86ff8e8
cuhte3 <farid@getportal.co>
2023-04-10 20:42:13 +0700

    Update TransactionRecord.swift
    init(payment: LightningPayment) added
    BitcoinDevKit.TransactionDetails parsing improvements

--- Portal/Models/TransactionRecord.swift
+++ Portal/Models/TransactionRecord.swift
@@ -39,15 +39,30 @@ struct TransactionRecord: Identifiable {
     
     init(transaction: BitcoinDevKit.TransactionDetails) {
         self.id = transaction.txid
-        self.type = transaction.sent > 0 ? .sent : .received
         
-        switch self.type {
-        case .sent:
-            self.amount = Decimal(transaction.sent)
-        case .received:
+        let sent = transaction.sent
+        let received = transaction.received
+        
+        if sent > 0, received > 0 {
+            self.type = sent > received ? .sent : .received
+            
+            switch type {
+            case .sent:
+                self.amount = Decimal(sent - received)
+            case .received:
+                self.amount = Decimal(received - sent)
+            case .swapped:
+                fatalError("should not happen")
+            }
+        } else if sent == 0, received > 0 {
+            self.type = .received
             self.amount = Decimal(transaction.received)
-        case .swapped:
-            self.amount = nil
+        } else if sent > 0, received == 0 {
+            self.type = .sent
+            self.amount = Decimal(transaction.sent)
+        } else {
+            self.type = .sent
+            self.amount = 0
         }
         
         if let blockInfo = transaction.confirmationTime {
@@ -91,18 +106,32 @@ struct TransactionRecord: Identifiable {
         preimage = nil
         nodeId = nil
     }
-    
-    init(invoice: Invoice, result: LightningPaymentResult) {
-        self.id = result.paymentID
-        self.type = .sent
+        
+    init(payment: LightningPayment) {
+        self.id = payment.paymentId
+        self.type = payment.type == .sent ? .sent : .received
         self.source = .lightning
-        self.timestamp = Int(Date().timeIntervalSince1970)
-        self.to = invoice.payment_secret().toHexString()
-        self.from = nil
-        self.amount = Decimal((invoice.amount_milli_satoshis().getValue() ?? 0))/1000
-        self.fee = Decimal(result.fee)/1000/100_000_000
-        self.preimage = result.preimage
-        self.nodeId = result.paymentHash
+        self.timestamp = payment.timestamp
+        
+        switch payment.type {
+        case .sent:
+            self.from = payment.nodeId
+            self.to = nil
+        case .received:
+            self.from = nil
+            self.to = payment.nodeId
+        }
+        
+        self.amount = Decimal(payment.amount)
+        
+        if let paymentFee = payment.fee {
+            self.fee = Decimal(paymentFee)/1000/100_000_000
+        } else {
+            self.fee = nil
+        }
+        
+        self.preimage = payment.preimage
+        self.nodeId = payment.nodeId
         self.blockHeight = nil
     }
 }
@@ -124,6 +153,16 @@ extension TransactionRecord {
     
     static var mockedLightning: TransactionRecord {
         let invoice = Invoice.from_str(s: "lntb150u1pjpm2rwpp5qtqkpsfupwnl5cm0jvd7v8asa3qd5y8kc2l2e3ua6v5dlszkzgyqdqqcqzpgxqyz5vqsp5kspjm0vlt6xhp5q6e78cp66e2fdx0lzg57ktf6kf30423qagstcq9qyyssq4l53zjvsqyc76ps7drxe2xjes2uvphh4ujr8dxpggx0sxcxtaa8q8f26k786gwrnususx5kcufr5gv5ktvj9d4vu9v8a2jehjhkv90spd6j4r3").getValue()!
-        return TransactionRecord(invoice: invoice, result: LightningPaymentResult(paymentID: "hdyvu5uumvyt7j5twkpp55eham28a4cnwz3epal2geeceskmjs6pxp", paymentHash: "pu5f4pjt7u7eps9qyysgqcqpcxqzlgsjl4nkpvgu4x54unwafr3s0h2mjtakw9cdklsa28qqdhmsxuqphhytyxlzwfx08nuwez5qvrvc", preimage: "lntb255m1p3l3qgadqqnp4qffgdax9g9ux3496d809u6le05nffsccvyuhdyvu5uumvyt7j5twkpp55eham28a4cnwz3epal2geeceskmjs6pxph987shgj3ydnv0nnqvssp5u8meh0nx9jaz68n97h3c22vxvmla2yynjgtcccpu5f4pjt7u7eps9qyysgqcqpcxqzlgsjl4nkpvgu4x54unwafr3s0h2mjtakw9cdklsa28qqdhmsxuqphhytyxlzwfx08nuwez5qvrvcfvkdgwdurtn67cr4l9wkdd6yu8rqgp88xwlu", fee: 1000))
+
+        let payment = LightningPayment(
+            nodeId: "hdyvu5uumvyt7j5twkpp55eham28a4cnwz3epal2geeceskmjs6pxp",
+            paymentId: "hdyvu5uumvyt7j5twkpp55eham28a4cnwz3epal2geeceskmjs6pxp",
+            amount: 100000,
+            preimage: "lntb255m1p3l3qgadqqnp4qffgdax9g9ux3496d809u6le05nffsccvyuhdyvu5uumvyt7j5twkpp55eham28a4cnwz3epal2geeceskmjs6pxph987shgj3ydnv0nnqvssp5u8meh0nx9jaz68n97h3c22vxvmla2yynjgtcccpu5f4pjt7u7eps9qyysgqcqpcxqzlgsjl4nkpvgu4x54unwafr3s0h2mjtakw9cdklsa28qqdhmsxuqphhytyxlzwfx08nuwez5qvrvcfvkdgwdurtn67cr4l9wkdd6yu8rqgp88xwlu",
+            type: .sent,
+            timestamp: Int(Date().timeIntervalSince1970),
+            fee: 1000
+        )
+        return TransactionRecord(payment: payment)
     }
 }
