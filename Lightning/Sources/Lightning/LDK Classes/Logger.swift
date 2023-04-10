dd00ca80815ce4ce6e283c3797cbf8714e10479d
cuhte3 <farid@getportal.co>
2023-04-10 21:14:54 +0700

    Update Logger.swift
    ldk logger output improvements

--- Lightning/Sources/Lightning/LDK Classes/Logger.swift
+++ Lightning/Sources/Lightning/LDK Classes/Logger.swift
@@ -12,22 +12,28 @@ class Logger: LightningDevKit.Logger {
     override func log(record: Bindings.Record) {
         let messageLevel = record.get_level()
         let arguments = record.get_args()
-        
+                
         switch messageLevel {
         case LDKLevel_Debug:
-            print("\nDebug Logger:\n>\(arguments)\n")
+            print("LDK LOG - Debug:")
+            print("\(arguments)\n")
         case LDKLevel_Info:
-            print("\nInfo Logger:\n>\(arguments)\n")
+            print("LDK LOG - Info:")
+            print("\(arguments)\n")
         case LDKLevel_Warn:
-            print("\nWarn Logger:\n>\(arguments)\n")
+            print("LDK LOG - Warn:")
+            print("\(arguments)\n")
         case LDKLevel_Error:
-            print("\nError Logger:\n>\(arguments)\n")
+            print("LDK LOG - Error:")
+            print("\(arguments)\n")
         case LDKLevel_Gossip:
             break //print("\nGossip Logger:\n>\(arguments)\n")
         case LDKLevel_Trace:
-            print("\nTrace Logger:\n>\(arguments)\n")
+            print("LDK LOG - Trace:")
+            print("\(arguments)\n")
         default:
-            print("\nUnknown Logger:\n>\(arguments)\n")
+            print("LDK LOG - Unknown:")
+            print("\(arguments)\n")
         }
     }
 }
