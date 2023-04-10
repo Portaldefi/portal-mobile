1871d5314c610f8dd26d2f958c2ddec00a9c3794
cuhte3 <farid@getportal.co>
2023-04-10 21:32:16 +0700

    Update Peer.swift
    Polar peers added

--- Portal/Models/Peer.swift
+++ Portal/Models/Peer.swift
@@ -42,6 +42,45 @@ class Peer: Codable, Equatable {
     }
 }
 
+extension Peer {
+    static var alice: Peer {
+        let name = "Alice"
+        let pubKey = "02d47008d34904e4949eb875e00720bd2dfbdbdccf71f38f2822b1697f204d63d9"
+        let host = "127.0.0.1"
+        let port: UInt16 = 9758
+                
+        return Peer(
+            peerPubKey: pubKey,
+            name: name,
+            connectionInformation: .init(hostname: host, port: port)
+        )
+    }
+    static var bob: Peer {
+        let name = "Bob"
+        let pubKey = "034ce84cb93a0ecb190cd5b347fd7e9b42d9bd6e1d58f24b6e5518d1ca03da9cb2"
+        let host = "127.0.0.1"    //P2p external
+        let port: UInt16 = 9759   //in Polar
+                
+        return Peer(
+            peerPubKey: pubKey,
+            name: name,
+            connectionInformation: .init(hostname: host, port: port)
+        )
+    }
+    static var carol: Peer {
+        let name = "Carol"
+        let pubKey = "03e0c667a8cb90e826e4ccf0db3383ddbf6cef207027c9dc5e3826315c5a822ed8"
+        let host = "127.0.0.1"    //P2p external
+        let port: UInt16 = 9760   //in Polar
+                
+        return Peer(
+            peerPubKey: pubKey,
+            name: name,
+            connectionInformation: .init(hostname: host, port: port)
+        )
+    }
+}
+
 // MARK: Helper Models
 extension Peer {
     struct PeerConnectionInformation: Codable {
