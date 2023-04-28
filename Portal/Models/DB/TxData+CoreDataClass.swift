1a03697751942d87a2d2df7aaa4c1ba86c1a489a
cuhte3 <farid@getportal.co>
2023-04-28 17:26:48 +0700

    TxData db model

---
+++ Portal/Models/DB/TxData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TxData+CoreDataClass.swift
+//  Portal
+//
+//  Created by farid on 4/28/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TxData)
+public class TxData: NSManagedObject {
+
+}
