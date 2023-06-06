41ebc837987403f997cda250cd6c8396595ba42a
cuhte3 <farid@getportal.co>
2023-06-06 18:29:27 +0700

    Update CoinManager.swift

--- Portal/Core/Manager/CoinManager.swift
+++ Portal/Core/Manager/CoinManager.swift
@@ -8,9 +8,11 @@
 import Foundation
 import SwiftUI
 import Combine
+import Factory
 
 final class CoinManager: ICoinManager {
     var onCoinsUpdate = PassthroughSubject<[Coin], Never>()
+    @Injected(Container.settings) private var settings
     
     private let storage: ICoinStorage
     private let accountManager: IAccountManager
@@ -46,34 +48,56 @@ final class CoinManager: ICoinManager {
     }
     
     private func syncCoins(account: Account?) {
-        guard let account = account else { return }
+//        guard let account = account else { return }
         
         walletCoins.removeAll()
         
-//        for code in account.coins.sorted(by: { $0 < $1 }) {
-//            switch code {
-//            case "BTC":
-//                walletCoins.append(.bitcoin())
-//            case "ETH":
-//                walletCoins.append(.ethereum())
-//            default:
-//                guard let coin = avaliableCoins.first(where: { $0.code == code }) else { return }
-//                walletCoins.append(coin)
-//            }
-//        }
+        walletCoins.append(.bitcoin())
+        walletCoins.append(.ethereum())
+        
+        for code in settings.userCoins {
+            guard let coin = avaliableCoins.first(where: { $0.code == code }) else { return }
+            walletCoins.append(coin)
+        }
         
         onCoinsUpdate.send(walletCoins)
-    }
-    
-    private func sadas() {
-        
     }
 }
 
 class CoinManagerMocked: ICoinManager {
     var onCoinsUpdate = PassthroughSubject<[Coin], Never>()
     var walletCoins: [Coin] = [.bitcoin(), .ethereum()]
-    var avaliableCoins: [Coin] = [.bitcoin()]
+    
+    var avaliableCoins: [Coin] = [
+        Coin(
+            type: .erc20(address: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB"),
+            code: "LINK",
+            name: "Chainlink Token",
+            decimal: 18,
+            iconUrl: "https://images.cointelegraph.com/images/1200_aHR0cHM6Ly9zMy5jb2ludGVsZWdyYXBoLmNvbS9zdG9yYWdlL3VwbG9hZHMvdmlldy9hNmRhMjI1NTRkMjBkZDdjNTg4NDM0N2IwMTcyN2ExMi5wbmc=.jpg"
+        ),
+        Coin(
+            type: .erc20(address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"),
+            code: "UNI",
+            name: "Uniswap Token",
+            decimal: 18,
+            iconUrl: "https://www.crypto-nation.io/cn-files/uploads/2021/01/Uniswap-Logo.png"
+        ),
+        Coin(
+            type: .erc20(address: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52"),
+            code: "BNB",
+            name: "Binance Token",
+            decimal: 18,
+            iconUrl: "https://www.crypto-nation.io/cn-files/uploads/2021/01/Uniswap-Logo.png"
+        ),
+        Coin(
+            type: .erc20(address: "0xc00e94Cb662C3520282E6f5717214004A7f26888"),
+            code: "COMP",
+            name: "Compound",
+            decimal: 18,
+            iconUrl: "https://www.crypto-nation.io/cn-files/uploads/2021/01/Uniswap-Logo.png"
+        )
+    ]
     
     init() {}
 }
