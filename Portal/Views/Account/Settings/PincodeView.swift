a0f02b0187c11c8b26c600343d0296e93233e8dd
cuhte3 <farid@getportal.co>
2023-06-29 00:48:41 +0700

    Create PincodeView.swift

---
+++ Portal/Views/Account/Settings/PincodeView.swift
@@ -0,0 +1,66 @@
+//
+//  PincodeView.swift
+//  Portal
+//
+//  Created by farid on 14.06.2023.
+//
+
+import SwiftUI
+import PortalUI
+
+struct PincodeView: View {
+    @StateObject private var viewModel = PincodeViewModel()
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+            
+            VStack(spacing: 25) {
+                Text("Enter your PIN")
+                    .font(.Main.fixed(.monoBold, size: 26))
+                    .foregroundColor(Palette.grayScaleCA)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .transition(.scale.combined(with: .opacity))
+                
+                if viewModel.state == .notMatched {
+                    Text("Pin did not match, try again.")
+                        .frame(height: 88)
+                        .font(.Main.fixed(.monoRegular, size: 16))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.red)
+                        .transition(.scale.combined(with: .opacity))
+                } else {
+                    Spacer()
+                        .frame(height: 88)
+                }
+                
+                CircleProgressView(
+                    totalPages: 4,
+                    currentIndex: viewModel.pin.count
+                )
+                .frame(height: 25)
+            }
+            .padding(.horizontal, 20)
+            
+            NumpadView(onAdd: {
+                viewModel.add(digit: $0)
+            }, onRemove: {
+                viewModel.removeLast()
+            })
+            .allowsHitTesting(viewModel.state == .notMatched ? false : true)
+            .opacity(viewModel.state == .notMatched ? 0.8 : 1)
+            .padding(.top, 65)
+            
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .blur(radius: viewModel.requiredBiometrics ? 8 : 0)
+    }
+}
+
+struct PincodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PincodeView()
+    }
+}
