d420d58e34c72c4bb0f7955db66246047173249d
cuhte3 <farid@getportal.co>
2023-06-29 00:37:07 +0700

    Update PortalSettings.swift

--- Portal/Models/PortalSettings.swift
+++ Portal/Models/PortalSettings.swift
@@ -11,32 +11,107 @@ import Combine
 class PortalSettings: ObservableObject {
     @Published var fiatCurrency = FiatCurrency(code: "USD") {
         didSet {
-            updateFiatCurrencyData()
+            updateFiatCurrencySetting()
         }
     }
     
-    @Published var portfolioCurrency = Coin.bitcoin()
+    @Published var userCoins = [String]() {
+        didSet {
+            updateUserCoinsSetting()
+        }
+    }
     
-    @Preference(\.fiatCurrencyData) private var fiatCurrencyData
+    @Published var portfolioCurrency = Coin.bitcoin() {
+        didSet {
+            updatePortfolioCurrencySetting()
+        }
+    }
+    
+    @Published var pincodeEnabled = false {
+        didSet {
+            updatePinCodeSetting()
+        }
+    }
+    
+    @Published var biometricsEnabled = false {
+        didSet {
+            updateBiometricsSetting()
+        }
+    }
+    
+    @Preference(\.fiatCurrencyData) private var fiatCurrencyPreference
+    @Preference(\.portfolioCurrencyData) private var portfolioCurrencyPreference
+    @Preference(\.userCoins) private var userCoinsPreference
+    @Preference(\.pincodeEnabled) private var pincodeEnabledPreference
+    @Preference(\.biometricsEnabled) private var biometricsEnabledPreference
     
     private var subscriptions = Set<AnyCancellable>()
     
     init() {
         guard
-            let data = fiatCurrencyData.data(using: .utf8),
+            let data = fiatCurrencyPreference.data(using: .utf8),
             let decoded = try? JSONDecoder().decode(FiatCurrency.self, from: data)
         else { return }
         
         fiatCurrency = decoded
+        
+        switch portfolioCurrencyPreference {
+        case "ETH":
+            portfolioCurrency = .ethereum()
+        default:
+            break
+        }
+        
+        userCoins = userCoinsPreference
+        pincodeEnabled = pincodeEnabledPreference
+        biometricsEnabled = biometricsEnabledPreference
+        
+        Preferences.standard
+            .preferencesChangedSubject
+            .filter { changedKeyPath in
+                changedKeyPath == \Preferences.biometricsEnabled
+            }.sink { [weak self] _ in
+                guard let self = self else { return }
+                self.biometricsEnabled = self.biometricsEnabledPreference
+            }
+            .store(in: &subscriptions)
+        
+        Preferences.standard
+            .preferencesChangedSubject
+            .filter { changedKeyPath in
+                changedKeyPath == \Preferences.pincodeEnabled
+            }.sink { [weak self] _ in
+                guard let self = self else { return }
+                self.pincodeEnabled = self.pincodeEnabledPreference
+            }
+            .store(in: &subscriptions)
     }
     
-    private func updateFiatCurrencyData() {
+    private func updateFiatCurrencySetting() {
         guard
             let encodedCurrencyData = try? JSONEncoder().encode(fiatCurrency),
             let encodedString = String(data: encodedCurrencyData, encoding: .utf8),
-            fiatCurrencyData != encodedString
+            fiatCurrencyPreference != encodedString
         else { return }
         
-        self.fiatCurrencyData = encodedString
+        self.fiatCurrencyPreference = encodedString
+    }
+    
+    private func updatePortfolioCurrencySetting() {
+        portfolioCurrencyPreference = portfolioCurrency.code
+    }
+    
+    private func updateUserCoinsSetting() {
+        userCoinsPreference = userCoins
+    }
+    
+    private func updatePinCodeSetting() {
+        guard pincodeEnabledPreference != pincodeEnabled else { return }
+        pincodeEnabledPreference = pincodeEnabled
+    }
+    
+    private func updateBiometricsSetting() {
+        guard biometricsEnabledPreference != biometricsEnabled else { return }
+        biometricsEnabledPreference = biometricsEnabled
     }
 }
