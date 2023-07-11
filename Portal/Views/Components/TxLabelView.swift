0cfabd0dfd87b9a2656f349b7cbff8b840c0db0e
cuhte3 <farid@getportal.co>
2023-07-11 16:27:43 +0700

    Update TxLabelView.swift
    Text replaced with HighlightedText

--- Portal/Views/Components/TxLabelView.swift
+++ Portal/Views/Components/TxLabelView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 import PortalUI
 
 struct TxLabelView: View {
+    var searchContext: String? = nil
     let label: TxLabel
     
     var body: some View {
-        Text(label.label)
-            .font(.Main.fixed(.monoMedium, size: 14))
-            .foregroundColor(Palette.grayScaleF4)
+        if searchContext?.lowercased() == label.label.lowercased() {
+            Text(label.label)
+                .font(.Main.fixed(.monoMedium, size: 14))
+                .foregroundColor(.black)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(Color(red: 255/255, green: 189/255, blue: 20/255, opacity: 1))
+                .cornerRadius(8)
+        } else {
+            HighlightedText(
+                text: label.label,
+                textColor: Palette.grayScaleF4,
+                highlight: searchContext,
+                font: .Main.fixed(.monoMedium, size: 14)
+            )
             .padding(.horizontal, 8)
             .padding(.vertical, 4)
             .background(Palette.grayScale4A)
             .cornerRadius(8)
+        }
     }
 }
 
