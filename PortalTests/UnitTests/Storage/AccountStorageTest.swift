d7c89b026e60c163508ca171a93d81e14d1a4aa1
cuhte3 <farid@getportal.co>
2023-07-14 19:36:03 +0700

    project files rearranged

---
+++ PortalTests/UnitTests/Storage/AccountStorageTest.swift
@@ -0,0 +1,66 @@
+//
+//  AccountStorageTest.swift
+//  PortalTests
+//
+//  Created by farid on 14.07.2023.
+//
+
+import XCTest
+@testable import Portal
+
+final class AccountStorageTest: XCTestCase {
+    
+//    var sut: AccountStorage!
+//    var localStorage: MockLocalStorage!
+//    var secureStorage: IKeychainStorage!
+//    var accountStorage: IAccountStorage!
+//
+//    override func setUp() {
+//        super.setUp()
+//        localStorage = MockLocalStorage()
+//        secureStorage = KeychainStorage.mocked
+//        accountStorage = AccountStorage.mocked
+//        sut = AccountStorage(localStorage: localStorage, secureStorage: secureStorage, accountStorage: accountStorage)
+//    }
+//
+//    override func tearDown() {
+//        sut = nil
+//        localStorage = nil
+//        secureStorage = nil
+//        accountStorage = nil
+//        super.tearDown()
+//    }
+//
+//    func testActiveAccountRecoveryData() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+//
+//    func testActiveAccount() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+//
+//    func testAllAccounts() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+//
+//    func testSaveAccount() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+//
+//    func testDeleteAccount() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+//
+//    func testClear() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+//
+//    func testSetCurrentAccount() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+//
+//    func testUpdateAccount() {
+//        // TODO: Set up your mock objects, call the function, and assert the results
+//    }
+    
+}
