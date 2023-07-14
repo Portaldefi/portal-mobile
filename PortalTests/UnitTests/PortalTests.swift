38690078d9bcc86ec0e6e5c025163fd3e3cb7b52
cuhte3 <farid@getportal.co>
2023-07-14 16:11:55 +0700

    PeerStoreTests

--- PortalTests/UnitTests/PortalTests.swift
+++ PortalTests/UnitTests/PortalTests.swift
@@ -6,23 +6,71 @@
 //
 
 import XCTest
+@testable import Portal
 
-final class PortalTests: XCTestCase {
+final class PeerStoreTests: XCTestCase {
+    
+    var testPeer: Peer!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        
+        let name = "TestPeer"
+        let pubKey = UUID().uuidString.replacingOccurrences(of: "-", with: "").lowercased()
+        let host = "127.0.0.2"
+        let port: UInt16 = 9742
+                
+        testPeer = Peer(
+            peerPubKey: pubKey,
+            name: name,
+            connectionInformation: .init(hostname: host, port: port)
+        )
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        PeerStore.clearPeersDocumentsDirectory()
+        testPeer = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testSave() {
+        let expectation = XCTestExpectation(description: "Saving peers")
+        
+        PeerStore.save(peers: [testPeer]) { result in
+            switch result {
+            case .success(let count):
+                XCTAssertEqual(count, 1, "Expected to save 1 peer.")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Save failed with error: \(error)")
+            }
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testLoad() throws {
+        //saving one peer
+        PeerStore.save(peers: [testPeer]) { _ in }
+        
+        let expectation = XCTestExpectation(description: "Loading peers")
+        
+        PeerStore.load { result in
+            switch result {
+            case .success(let peers):
+                XCTAssertTrue(peers.contains(where: { (key: String, value: Peer) in
+                    self.testPeer == value
+                }))
+                expectation.fulfill()
+            case .failure(let error):
+                XCTFail("Load failed with error: \(error)")
+            }
+        }
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testClearPeersDocumentsDirectory() {
+        PeerStore.clearPeersDocumentsDirectory()
+        // TODO: Check if the directory was cleared
     }
 
     func testPerformanceExample() throws {
