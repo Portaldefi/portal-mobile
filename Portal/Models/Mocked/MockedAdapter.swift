e88b12d40fc7e402664adfcb3d4d45fd6bd86540
cuhte3 <farid@getportal.co>
2023-07-19 19:01:09 +0700

    Create MockedAdapter.swift

---
+++ Portal/Models/Mocked/MockedAdapter.swift
@@ -0,0 +1,74 @@
+//
+//  MockedAdapter.swift
+//  Portal
+//
+//  Created by farid on 2/21/23.
+//
+
+import Foundation
+import Combine
+import BitcoinDevKit
+
+class MockedAdapter: IAdapter, ISendBitcoinAdapter, ITransactionsAdapter, IDepositAdapter, IBalanceAdapter {
+    var pubKey: String {
+        "tb1q3ds30e5p59x9ryee4e2kxz9vxg5ur0tjsv0ug3"
+    }
+    
+    var L1Balance: Decimal { 0.003 }
+    
+    func send(amount: Decimal, address: String) throws -> TransactionRecord {
+        TransactionRecord.mocked
+    }
+    
+    var state: AdapterState = .synced
+    
+    var balanceStateUpdated: AnyPublisher<Void, Never> {
+        Just(()).eraseToAnyPublisher()
+    }
+    
+    var balanceUpdated: AnyPublisher<Void, Never> {
+        Just(()).eraseToAnyPublisher()
+    }
+    
+    var receiveAddress: String = "tb1q3ds30e5p59x9ryee4e2kxz9vxg5ur0tjsv0ug3"
+        
+    var balance: Decimal = 1
+    
+    func validate(address: String) throws {
+        
+    }
+    
+    func fee(max: Bool, address: String, amount: Decimal, fee: Int?) throws -> UInt64? {
+        1000
+    }
+    
+    func send(amount: Decimal, address: String, fee: Int?) throws -> TransactionRecord {
+        TransactionRecord.mocked
+    }
+    
+    func sendMax(address: String, fee: Int?) throws -> TransactionRecord {
+        TransactionRecord.mocked
+    }
+    
+    func rawTransaction(amount: UInt64, address: String) throws -> Transaction {
+        try! Transaction(transactionBytes: [])
+    }
+    
+    func start() {
+        
+    }
+    
+    func stop() {
+        
+    }
+    
+    func refresh() {
+        
+    }
+    
+    var transactionRecords: [TransactionRecord] {
+        [TransactionRecord.mocked]
+    }
+    
+    var blockchainHeight: Int32 = 0
+}
