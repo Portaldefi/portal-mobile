80d8ee432bc17ea99b41a9b21cbf33e980d87fd3
cuhte3 <farid@getportal.co>
2023-07-19 19:01:20 +0700

    Create MockedPortalSettings.swift

---
+++ Portal/Models/Mocked/MockedPortalSettings.swift
@@ -0,0 +1,37 @@
+//
+//  MockedPortalSettings.swift
+//  Portal
+//
+//  Created by farid on 19.07.2023.
+//
+
+import Foundation
+import Combine
+
+struct MockedPortalSettings: IPortalSettings {
+    private(set) var fiatCurrency: CurrentValueSubject<FiatCurrency, Never> = .init(FiatCurrency(code: "USD"))
+    private(set) var userCoins: CurrentValueSubject<[String], Never> = .init([String]())
+    private(set) var portfolioCurrency: CurrentValueSubject<Coin, Never> = .init(.bitcoin())
+    private(set) var pincodeEnabled: CurrentValueSubject<Bool, Never> = .init(false)
+    private(set) var biometricsEnabled: CurrentValueSubject<Bool, Never> = .init(false)
+    
+    func updateFiatCurrency(_ currency: FiatCurrency) {
+        fiatCurrency.send(currency)
+    }
+    
+    func updatePortfolioCurrency(_ coin: Coin) {
+        portfolioCurrency.send(coin)
+    }
+    
+    func updateUserCoins(_ codes: [String]) {
+        userCoins.send(codes)
+    }
+    
+    func updatePinCodeSetting(enabled: Bool) {
+        pincodeEnabled.send(enabled)
+    }
+    
+    func updateBiometricsSetting(enabled: Bool) {
+        biometricsEnabled.send(enabled)
+    }
+}
