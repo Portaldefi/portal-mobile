7ab5dda37f397f95ba192aa7d10aea7107c88bdd
cuhte3 <farid@getportal.co>
2023-09-12 19:56:29 +0700

    Update CoinTest.swift

--- PortalTests/UnitTests/Models/CoinTest.swift
+++ PortalTests/UnitTests/Models/CoinTest.swift
@@ -45,7 +45,7 @@ final class CoinTest: XCTestCase {
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            testCoinProperties()
         }
     }
 
