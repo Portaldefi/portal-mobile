9c6c97d156850b8809d28dc83310cc5b281091d7
cuhte3 <farid@getportal.co>
2023-10-09 21:19:56 +0700

    Update RootViewViewModel.swift

--- Portal/ViewModels/RootViewViewModel.swift
+++ Portal/ViewModels/RootViewViewModel.swift
@@ -9,16 +9,22 @@ import Foundation
 import Combine
 import Factory
 
-class RootViewViewModel: ObservableObject {
+@Observable class RootViewViewModel {
     enum State {
         case account, empty
     }
-    @Published var state: State = .empty
+    public var state: State = .empty
     
-    private var subscriptions = Set<AnyCancellable>()
-    @Injected(Container.accountManager) private var manager
+    @ObservationIgnored private var subscriptions = Set<AnyCancellable>()
+    @ObservationIgnored private var manager = Container.accountManager()
+    @ObservationIgnored private var settings = Container.settings()
 
     init() {
+//        if let address = getWiFiAddress() {
+//            print("Local network address: \(address)")
+//            UserDefaults.standard.set(address, forKey: "LocalNetworkIpAddress")
+//        }
+        
         if manager.activeAccount != nil {
             state = .account
         }
@@ -30,5 +36,27 @@ class RootViewViewModel: ObservableObject {
                 state = .account
             }
             .store(in: &subscriptions)
+    }
+    
+    func getWiFiAddress() -> String? {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return nil }
+        defer { freeifaddrs(ifaddr) }
+
+        for ptr in sequence(first: ifaddr, next: { $0?.pointee.ifa_next }) {
+            guard
+                let interface = ptr?.pointee.ifa_name,
+                let addr = ptr?.pointee.ifa_addr,
+                addr.pointee.sa_family == UInt8(AF_INET),
+                String(cString: interface) == "en0"
+            else { continue }
+
+            var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+            getnameinfo(addr, socklen_t(addr.pointee.sa_len), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST)
+            address = String(cString: hostname)
+        }
+
+        return address
     }
 }
