384b5b9f16dab97150dd860bd7163e92c3632463
cuhte3 <farid@getportal.co>
2024-01-04 14:26:34 +0700

    Update RootViewViewModel.swift

--- Portal/ViewModels/RootViewViewModel.swift
+++ Portal/ViewModels/RootViewViewModel.swift
@@ -20,11 +20,6 @@ import Factory
     @ObservationIgnored private var settings = Container.settings()
 
     init() {
-//        if let address = getWiFiAddress() {
-//            print("Local network address: \(address)")
-//            UserDefaults.standard.set(address, forKey: "LocalNetworkIpAddress")
-//        }
-        
         if manager.activeAccount != nil {
             state = .account
         }
@@ -36,27 +31,5 @@ import Factory
                 state = .account
             }
             .store(in: &subscriptions)
-    }
-    
-    func getWiFiAddress() -> String? {
-        var address: String?
-        var ifaddr: UnsafeMutablePointer<ifaddrs>?
-        guard getifaddrs(&ifaddr) == 0 else { return nil }
-        defer { freeifaddrs(ifaddr) }
-
-        for ptr in sequence(first: ifaddr, next: { $0?.pointee.ifa_next }) {
-            guard
-                let interface = ptr?.pointee.ifa_name,
-                let addr = ptr?.pointee.ifa_addr,
-                addr.pointee.sa_family == UInt8(AF_INET),
-                String(cString: interface) == "en0"
-            else { continue }
-
-            var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-            getnameinfo(addr, socklen_t(addr.pointee.sa_len), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST)
-            address = String(cString: hostname)
-        }
-
-        return address
     }
 }
