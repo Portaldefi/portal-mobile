5531b64bb71b46b3dff0dde2b45fbca19273c0c0
cuhte3 <farid@getportal.co>
2024-01-05 21:51:09 +0700

    Update Utils.swift

--- Lightning/Sources/Lightning/Utils/Utils.swift
+++ Lightning/Sources/Lightning/Utils/Utils.swift
@@ -80,4 +80,35 @@ class Utils {
         }
         return bytes
     }
+    
+    static func hexStringToBytes(hexString: String) -> [UInt8]? {
+        let hexStr = hexString.dropFirst(hexString.hasPrefix("0x") ? 2 : 0)
+
+        guard hexStr.count % 2 == 0 else {
+            return nil
+        }
+
+        var newData = [UInt8]()
+
+        var indexIsEven = true
+        for i in hexStr.indices {
+            if indexIsEven {
+                let byteRange = i...hexStr.index(after: i)
+                guard let byte = UInt8(hexStr[byteRange], radix: 16) else {
+                    return nil
+                }
+                newData.append(byte)
+            }
+            indexIsEven.toggle()
+        }
+        return newData
+    }
+
+    static func bytesToHexString(bytes: [UInt8]) -> String {
+        let format = "%02hhx" // "%02hhX" (uppercase)
+        return bytes.map {
+            String(format: format, $0)
+        }
+        .joined()
+    }
 }
