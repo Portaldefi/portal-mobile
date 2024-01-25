c7b942c741b2dfdd46d25ccd1a44fcb007965e31
cuhte3 <farid@getportal.co>
2024-01-25 22:18:20 +0700

    Update Broadcaster.swift
    added check if tx included in blockchain before publishing

--- Lightning/Sources/Lightning/LDK Classes/Broadcaster.swift
+++ Lightning/Sources/Lightning/LDK Classes/Broadcaster.swift
@@ -8,33 +8,32 @@
 
 import Foundation
 import LightningDevKit
+import BitcoinDevKit
 
 class Broadcaster: BroadcasterInterface {
-    private let chainInterface: RpcChainManager
+    private let rpcInterface: RpcChainManager
     
     init(rpcInterface: RpcChainManager) {
-        self.chainInterface = rpcInterface
+        self.rpcInterface = rpcInterface
         super.init()
     }
     
     override func broadcastTransactions(txs: [[UInt8]]) {
         Task {
             for tx in txs {
-                if
-                    let json = try? await chainInterface.decodeRawTransaction(tx: tx),
-                    let result = json["result"] as? [String: Any],
-                    let txId = result["txid"] as? String  {
-                    print(txId)
-                    
-                    //                if let transaction = try? await chainInterface.getTransaction(with: txId) {
-                    //                    print("transaction: \(transaction.toHexString())")
-                    //                } else {
-                    let txID = try? await self.chainInterface.submitTransaction(transaction: tx)
-                    print("Submitted tx with id: \(String(describing: txID))")
-                    //                }
-                } else {
-                    let txID = try? await self.chainInterface.submitTransaction(transaction: tx)
-                    print("Submitted tx with id: \(String(describing: txID))")
+                let transaction = try Transaction(transactionBytes: tx)
+                let expectedTxId = transaction.txid()
+                let transactionDict = try await rpcInterface.getTransactionWithId(id: expectedTxId)
+                
+                guard transactionDict.keys.isEmpty else {
+                    break
+                }
+                
+                let resultTxId = try await self.rpcInterface.submitTransaction(transaction: transaction.serialize())
+                print("Submitted tx with id: \(resultTxId)")
+                
+                guard expectedTxId == resultTxId else {
+                    fatalError("Expected txId: \(expectedTxId), result: \(resultTxId)")
                 }
             }
         }
