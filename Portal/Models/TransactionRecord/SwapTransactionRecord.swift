8364bd2e71fbbde24a858e6aa82160117857da39
cuhte3 <farid@getportal.co>
2024-02-01 19:04:09 +0700

    Update SwapTransactionRecord.swift

--- Portal/Models/TransactionRecord/SwapTransactionRecord.swift
+++ Portal/Models/TransactionRecord/SwapTransactionRecord.swift
@@ -9,31 +9,39 @@ import Foundation
 import PortalSwapSDK
 
 class SwapTransactionRecord: TransactionRecord {
+    private let satsInBtc: Decimal = 100_000_000
+    private let weiInEth: Decimal = 1_000_000_000_000_000_000
+    
     let base: Coin
     let quote: Coin
     
     let baseQuantity: Decimal
     let quoteQuantity: Decimal
     
-    init(swap: DBSwap, userData: TxUserData) {
+    let party: PartyModel
+    let partyType: PartyType
+    
+    init(swap: SwapModel, userData: TxUserData) {
         switch swap.partyType {
-        case "holder":
-            baseQuantity = Decimal(swap.secretHolder?.quantity ?? 0) / 100_000_000
-            quoteQuantity = Decimal(swap.secretSeeker?.quantity ?? 0) / 1_000_000_000_000_000_000
+        case .secretHolder:
+            party = swap.secretHolder
+            baseQuantity = Decimal(swap.secretHolder.quantity) / satsInBtc
+            quoteQuantity = Decimal(swap.secretSeeker.quantity) / weiInEth
             base = .lightningBitcoin()
             quote = .ethereum()
-        case "seeker":
-            baseQuantity = Decimal(swap.secretSeeker?.quantity ?? 0) / 1_000_000_000_000_000_000
-            quoteQuantity = Decimal(swap.secretHolder?.quantity ?? 0) / 100_000_000
+        case .secretSeeker:
+            party = swap.secretSeeker
+            baseQuantity = Decimal(swap.secretSeeker.quantity) / weiInEth
+            quoteQuantity = Decimal(swap.secretHolder.quantity) / satsInBtc
             base = .ethereum()
             quote = .lightningBitcoin()
-        default:
-            fatalError("Unknown partyType")
         }
+        
+        partyType = swap.partyType
         
         let type: TxType = .swap(base: base, quote: quote)
         let source: TxSource = .swap(base: base, quote: quote)
         
-        super.init(source: source, type: type, id: swap.swapID!, timestamp: Int(swap.timestamp), userData: userData)
+        super.init(source: source, type: type, id: swap.swapId, timestamp: Int(swap.timestamp), userData: userData)
     }
 }
