53a6c469972db39630a6bc8db4ef079532d117b1
cuhte3 <farid@getportal.co>
2024-02-05 23:32:33 +0700

    Update WalletItemView.swift

--- Portal/Views/Components/WalletItemView.swift
+++ Portal/Views/Components/WalletItemView.swift
@@ -22,40 +22,51 @@ struct WalletItemView: View {
     }
     
     var body: some View {
-        HStack(alignment: .firstTextBaseline) {
-            VStack(spacing: 12) {
-                VStack(alignment: .leading, spacing: 4.2) {
-                    HStack(spacing: 8) {
+        switch viewModel.coin.type {
+        case .lightningBitcoin:
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack(spacing: 0) {
                         CoinImageView(
                             size: 32,
                             url: viewModel.coin.icon,
                             placeholderForegroundColor: Color.gray
                         )
+                        .padding(.trailing, 8)
                         
-                        VStack(alignment: .leading, spacing: 6) {
+                        VStack(alignment: .leading, spacing: 5) {
                             Text(viewModel.coin.name)
                                 .font(.Main.fixed(.monoBold, size: 16))
                                 .foregroundColor(.white)
                                 .frame(height: 16)
+                            
                             Text("\(viewModel.coin.code) • \(viewModel.coin.network)")
                                 .font(.Main.fixed(.monoRegular, size: 14))
                                 .foregroundColor(Palette.grayScale6A)
                         }
                     }
                 }
-            }
-            
-            Spacer()
-            
-            switch viewModel.coin.type {
-            case .lightningBitcoin:
-                if let channel = viewModel.lightningKit.allChannels.first, let confirmations = channel.getConfirmations(), let requiredConfirmations = channel.getConfirmationsRequired(), confirmations < requiredConfirmations {
+                
+                Spacer()
+                
+                if
+                    let channel = viewModel.lightningKit.allChannels.first,
+                    let confirmations = channel.getConfirmations(),
+                    let requiredConfirmations = channel.getConfirmationsRequired(),
+                    confirmations < requiredConfirmations
+                {
                     Text("Waiting for Confirmations")
                         .multilineTextAlignment(.center)
                         .font(.Main.fixed(.monoRegular, size: 16))
                         .foregroundColor(.yellow)
                         .padding(.trailing, 12)
-                } else if let channel = viewModel.lightningKit.allChannels.first, let confirmations = channel.getConfirmations(), let requiredConfirmations = channel.getConfirmationsRequired(), confirmations >= requiredConfirmations {
+                    
+                } else if
+                    let channel = viewModel.lightningKit.allChannels.first,
+                    let confirmations = channel.getConfirmations(),
+                    let requiredConfirmations = channel.getConfirmationsRequired(),
+                    confirmations >= requiredConfirmations
+                {
                     HStack(spacing: 6) {
                         VStack(alignment: .trailing, spacing: 0) {
                             Text(viewModel.balanceString)
@@ -63,18 +74,16 @@ struct WalletItemView: View {
                                 .foregroundColor(Palette.grayScaleEA)
                                 .minimumScaleFactor(0.5)
                                 .lineLimit(1)
+                            
                             Text(viewModel.valueString)
                                 .font(.Main.fixed(.monoMedium, size: 16))
                                 .foregroundColor(Palette.grayScale6A)
                         }
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack {
-                                Text(viewModel.coin.unit)
-                                    .font(.Main.fixed(.monoRegular, size: 14))
-                                    .foregroundColor(Palette.grayScaleAA)
-                                Spacer()
-                            }
-                            .frame(width: 42)
+                        VStack(alignment: .trailing, spacing: 0) {
+                            Text(viewModel.coin.unit)
+                                .font(.Main.fixed(.monoRegular, size: 14))
+                                .foregroundColor(Palette.grayScaleAA)
+                            
                             Text(viewModel.fiatCurrency.code)
                                 .font(.Main.fixed(.monoRegular, size: 14))
                                 .foregroundColor(Palette.grayScale6A)
@@ -89,50 +98,74 @@ struct WalletItemView: View {
                         .padding(.trailing, 12)
                         .frame(width: 80)
                 }
-            default:
-                if showBalance {
-                    HStack(spacing: 6) {
-                        VStack(alignment: .trailing, spacing: 0) {
-                            Text(viewModel.balanceString)
-                                .font(.Main.fixed(.monoBold, size: 20))
-                                .foregroundColor(Palette.grayScaleEA)
-                                .minimumScaleFactor(0.5)
-                                .lineLimit(1)
-                            Text(viewModel.valueString)
-                                .font(.Main.fixed(.monoMedium, size: 16))
-                                .foregroundColor(Palette.grayScale6A)
-                        }
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack {
+            }
+            .padding(.vertical, 12)
+            .frame(height: 70)
+        default:
+            VStack(alignment: .leading, spacing: 4) {
+                HStack(spacing: 0) {
+                    CoinImageView(
+                        size: 32,
+                        url: viewModel.coin.icon,
+                        placeholderForegroundColor: Color.gray
+                    )
+                    .padding(.trailing, 8)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        HStack(spacing: 0) {
+                            Text(viewModel.coin.name)
+                                .font(.Main.fixed(.monoBold, size: 16))
+                                .foregroundColor(.white)
+                                .frame(height: 16)
+                            
+                            Spacer()
+                            
+                            if showBalance {
+                                Text(viewModel.balanceString)
+                                    .font(.Main.fixed(.monoBold, size: 20))
+                                    .foregroundColor(Palette.grayScaleEA)
+                                    .minimumScaleFactor(0.5)
+                                    .lineLimit(1)
+                                
                                 Text(viewModel.coin.unit)
                                     .font(.Main.fixed(.monoRegular, size: 14))
                                     .foregroundColor(Palette.grayScaleAA)
-                                Spacer()
+                                    .padding(.leading, 4)
+                                    .offset(y: 1)
                             }
-                            .frame(width: 42)
-                            Text(viewModel.fiatCurrency.code)
+                        }
+                        
+                        HStack(spacing: 0) {
+                            Text("\(viewModel.coin.code) • \(viewModel.coin.network)")
                                 .font(.Main.fixed(.monoRegular, size: 14))
                                 .foregroundColor(Palette.grayScale6A)
-                                .offset(y: 3)
+                            
+                            Spacer()
+                            
+                            if showBalance {
+                                Text(viewModel.valueString)
+                                    .font(.Main.fixed(.monoMedium, size: 16))
+                                    .foregroundColor(Palette.grayScale6A)
+                                
+                                Text(viewModel.fiatCurrency.code)
+                                    .font(.Main.fixed(.monoRegular, size: 14))
+                                    .foregroundColor(Palette.grayScale6A)
+                                    .padding(.leading, 4)
+                                    .offset(y: 1)
+                            }
                         }
                     }
                 }
             }
-            
-//            if viewModel.coin == .lightningBitcoin() {
-//                
-//            } else {
-//
-//            }
+            .padding(.vertical, 12)
+            .frame(height: 70)
         }
-        .padding(.vertical, 12)
-        .frame(height: 70)
     }
 }
 
 struct WalletItemView_Previews: PreviewProvider {
     static var previews: some View {
-        WalletItemView(viewModel: WalletItem.mocked.viewModel)
+        WalletItemView(viewModel: WalletItem.mockedPortal.viewModel)
             .previewLayout(PreviewLayout.sizeThatFits)
             .padding()
     }
