18624794c74b2536640e16a8acba8b49321549b5
cuhte3 <farid@getportal.co>
2024-02-16 23:14:02 +0700

    Update IPortalSettings.swift

--- Portal/Protocols/IPortalSettings.swift
+++ Portal/Protocols/IPortalSettings.swift
@@ -14,10 +14,12 @@ protocol IPortalSettings {
     var portfolioCurrency: CurrentValueSubject<Coin, Never> { get }
     var pincodeEnabled: CurrentValueSubject<Bool, Never> { get }
     var biometricsEnabled: CurrentValueSubject<Bool, Never> { get }
+    var notificationsEnabled: CurrentValueSubject<Bool, Never> { get }
     
     func updateFiatCurrency(_ currency: FiatCurrency)
     func updatePortfolioCurrency(_ coin: Coin)
     func updateUserCoins(_ codes: [String])
     func updatePinCodeSetting(enabled: Bool)
     func updateBiometricsSetting(enabled: Bool)
+    func updateNotificationsSetting(enabled: Bool)
 }
